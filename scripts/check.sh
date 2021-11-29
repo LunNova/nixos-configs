f9c34640820f7f0c2b6d7d39b86bbc543ea35531
Luna Nova <git@nyx.nova.fail>
2021-11-29 05:49:47 -0800

    Build all derivations in scripts/check.sh

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-set -euo pipefail
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash jq
+set -xeuo pipefail
 
 nixos-rebuild build --flake .#
 nix build .#homeManagerConfigurations.lun.activationPackage
+nix flake show --json | jq '[leaf_paths as $path | select(getpath($path) == "derivation") | {"key": $path | join(".") | sub(".type";""), "value": getpath($path)}] | from_entries | keys|.[]' -cr | xargs -tI{} nix build .#{}
