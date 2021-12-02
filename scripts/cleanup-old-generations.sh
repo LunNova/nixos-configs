1605d5765c966a8fa454addde77f72e6031914dd
Luna Nova <git@nyx.nova.fail>
2021-12-02 04:35:35 -0800

    Add nix store optimisation to cleanup-old-generations.sh

--- scripts/cleanup-old-generations.sh
+++ scripts/cleanup-old-generations.sh
@@ -3,3 +3,6 @@ set -xeuo pipefail
 
 sudo nix-collect-garbage --delete-older-than 3d
 sudo /nix/var/nix/profiles/system/bin/switch-to-configuration boot
+
+# TODO autooptimise?
+nix-store --optimise
