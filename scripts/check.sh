2e7ec22c28ed5dfe32109d4374814dece714de00
Luna Nova <git@nyx.nova.fail>
2021-12-08 03:58:37 -0800

    Use scripts/check.sh in github actions

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,5 +2,10 @@
 # shellcheck shell=bash
 set -xeuo pipefail
 
+nix flake check
 nix flake show --json | jq 'del(.devShell) | [leaf_paths as $path | select(getpath($path) == "derivation") | {"key": $path | join(".") | sub(".type";""), "value": getpath($path)}] | from_entries | keys|.[]' -cr | xargs -tI{} nix build .#{}
-nixos-rebuild build --flake .#
+if [ -v 1 ] && [ "$1" == "all" ]; then
+    nix flake show --json | jq '.nixosConfigurations | keys[] as $k | select(.[$k].type=="nixos-configuration") | $k' -cr | xargs -tI{} nix build .#nixosConfigurations.{}.config.system.build.toplevel
+else
+    nixos-rebuild build --flake .#
+fi
