3b252a10b7aff99809069985d83e9309a33a9422
Luna Nova <git@nyx.nova.fail>
2021-12-17 11:29:42 -0800

    Use gdm with plasma5

--- modules/common.nix
+++ modules/common.nix
@@ -132,7 +132,10 @@
     # DESKTOP ENV
     # Enable the X11 windowing system.
     services.xserver.enable = true;
-    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.displayManager.sddm.enable = false;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.displayManager.gdm.wayland = true;
+    services.xserver.displayManager.gdm.nvidiaWayland = true;
     services.xserver.desktopManager.plasma5.enable = true;
     services.xserver.desktopManager.plasma5.runUsingSystemd = true;
 
