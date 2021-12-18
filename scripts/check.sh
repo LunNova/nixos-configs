159d7e3b91e6b3b027548a5534b2f52765ab1feb
Luna Nova <git@nyx.nova.fail>
2021-12-18 13:15:16 -0800

    Check script now uses no-link

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,9 +3,9 @@
 set -xeuo pipefail
 
 nix flake check
-nix flake show --json | jq 'del(.devShell) | [leaf_paths as $path | select(getpath($path) == "derivation") | {"key": $path | join(".") | sub(".type";""), "value": getpath($path)}] | from_entries | keys|.[]' -cr | xargs -tI{} nix build .#{}
+nix flake show --json | jq 'del(.devShell) | [leaf_paths as $path | select(getpath($path) == "derivation") | {"key": $path | join(".") | sub(".type";""), "value": getpath($path)}] | from_entries | keys|.[]' -cr | xargs -tI{} nix build --no-link .#{}
 if [ -v 1 ] && [ "$1" == "all" ]; then
-    nix flake show --json | jq '.nixosConfigurations | keys[] as $k | select(.[$k].type=="nixos-configuration") | $k' -cr | xargs -tI{} nix build .#nixosConfigurations.{}.config.system.build.toplevel
+    nix flake show --json | jq '.nixosConfigurations | keys[] as $k | select(.[$k].type=="nixos-configuration") | $k' -cr | xargs -tI{} nix build --no-link .#nixosConfigurations.{}.config.system.build.toplevel
 elif [ -f /etc/NIXOS ]; then
-    nixos-rebuild build --flake .#
+    nix build --no-link ".#nixosConfigurations.$(hostname).config.system.build.toplevel"
 fi
