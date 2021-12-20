0b2cbd78688079da3904b5b36bbea5cb4f0e7b87
Luna Nova <git@nyx.nova.fail>
2021-12-19 16:17:53 -0800

    Rename unfree nixpkgs added to registry

--- flake.nix
+++ flake.nix
@@ -98,8 +98,8 @@
           {
             # pin system nixpkgs to the same version as the flake input
             # (don't see a way to declaratively set channels but this seems to work fine?)
-            nix.registry.nixpkgs.flake = nixpkgs-unfree-relocked;
-            nix.nixPath = [ "nixpkgs=${nixpkgs-unfree-relocked}" ];
+            nix.registry.pkgs.flake = nixpkgs-unfree-relocked;
+            nix.nixPath = [ "pkgs=${nixpkgs-unfree-relocked}" ];
             system.configurationRevision = lib.mkIf (args.self ? rev) args.self.rev; # set configurationRevision if available
           }
           home-manager.nixosModules.home-manager
