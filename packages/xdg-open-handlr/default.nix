67e9e5849355ad71674ba15d185dbbaee1629bbc
Luna Nova <git@lunnova.dev>
2021-12-25 12:49:46 -0800

    Remove debug logging from xdg-open-handlr

--- packages/xdg-open-handlr/default.nix
+++ packages/xdg-open-handlr/default.nix
@@ -4,14 +4,9 @@
 pkgs.writeShellScriptBin "xdg-open" ''
   #!/bin/sh
 
-  echo "xdg-open-workaround: Launching $@"
-  echo "xdg-open-workaround: Launching $@" > ~/dev/xdg-open-log
-  export RUST_BACKTRACE=1
+  echo "xdg-open workaround: Launching $@"
 
-  cat /etc/profile > ~/dev/xdg-open-log
-  ls /etc/profiles/per-user/lun/share/applications > ~/dev/xdg-open-log
-
-  env -i USER="$USER" HOME="$HOME" \
+  env -i RUST_BACKTRACE=1 USER="$USER" HOME="$HOME" \
       XDG_DATA_HOME=$XDG_DATA_HOME XDG_DATA_DIRS=$XDG_DATA_DIRS XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR XAUTHORITY=$XAUTHORITY DISPLAY=$DISPLAY KDE_SESSION_VERSION=$KDE_SESSION_VERSION XDG_CURRENT_DESKTOP=X-Generic \
       bash -lc 'env | sort >> ~/dev/xdg-open-log && ${pkgs.lun.handlr}/bin/handlr open '"$@"
 ''
