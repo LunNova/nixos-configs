3e0b57946844792a4fc3627de0fac9725dbdf2ed
Luna Nova <git@lunnova.dev>
2021-12-24 22:37:18 -0800

    Check homeConfigurations

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,6 +4,8 @@ set -xeuo pipefail
 
 nix flake check
 nix flake show --json | jq 'del(.devShell) | [leaf_paths as $path | select(getpath($path) == "derivation") | {"key": $path | join(".") | sub(".type";""), "value": getpath($path)}] | from_entries | keys|.[]' -cr | xargs -tI{} nix build --no-link .#{}
+nix eval ".#homeConfigurations" --apply 'f: builtins.concatStringsSep "\n" (builtins.attrNames f)' --raw | \
+  xargs -tI{} nix build --no-link .#homeConfigurations.{}.activationPackage
 if [ -v 1 ] && [ "$1" == "all" ]; then
     nix flake show --json | jq '.nixosConfigurations | keys[] as $k | select(.[$k].type=="nixos-configuration") | $k' -cr | xargs -tI{} nix build --no-link .#nixosConfigurations.{}.config.system.build.toplevel
 elif [ -f /etc/NIXOS ]; then
