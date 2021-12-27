25aac322a29cb7484207d5b32ce721f512d17642
Luna Nova <git@lunnova.dev>
2021-12-27 10:46:52 -0800

    Enable xdg.portal.gtkUsePortal in default config

--- modules/common.nix
+++ modules/common.nix
@@ -140,6 +140,7 @@
     # Enable the X11 windowing system.
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = false;
+    xdg.portal.gtkUsePortal = true; # Use xdg-desktop-portal for file pickers
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.displayManager.gdm.wayland = true;
     services.xserver.displayManager.gdm.nvidiaWayland = true;
