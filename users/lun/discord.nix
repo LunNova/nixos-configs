5cd04e7003ded37e1de226345d42b280120fa410
Luna Nova <git@lunnova.dev>
2021-12-31 15:35:59 -0800

    Add TODO for discord accel workaround

--- users/lun/discord.nix
+++ users/lun/discord.nix
@@ -2,6 +2,9 @@
 {
   home.packages = [
     (pkgs.discord-plugged.override {
+      # TODO make it launch with 
+      # discordcanary --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
+      discord-canary = pkgs.discord-canary.override { };
       powercord = pkgs.lun.powercord;
       plugins = pkgs.powercord-plugins;
       themes = pkgs.powercord-themes;
