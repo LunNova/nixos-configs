645be75325c0d26baba15dc22d405528cd81669c
Luna Nova <git@lunnova.dev>
2021-12-31 21:58:05 -0800

    Remove less used caches

--- modules/nix-binary-caches.nix
+++ modules/nix-binary-caches.nix
@@ -2,18 +2,13 @@
   config.nix = {
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org/"
       "https://lun-nixos-configs.cachix.org/"
-      # https://discourse.nixos.org/t/content-addressed-nix-call-for-testers/12881/46
-      "https://cache.ngi0.nixos.org/"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "lun-nixos-configs.cachix.org-1:4jBzwzEn7vbyrnTMz9lc5JF8fYBTIT6gWDiutrkxmDU="
-      "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
     ];
   };
 }
