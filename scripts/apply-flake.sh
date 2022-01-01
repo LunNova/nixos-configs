ea378af78fb3fffd695a94c7a7caf1541550dc3f
Luna Nova <git@lunnova.dev>
2021-12-31 18:45:15 -0800

    Speed up apply-flake script for specialisations

--- scripts/apply-flake.sh
+++ scripts/apply-flake.sh
@@ -5,8 +5,15 @@ cd "$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
 
 if [[ -v 1 ]]; then
     echo "Activating spec $1"
-    nixos-rebuild build --flake ".#" && sudo "result/specialisation/$1/bin/switch-to-configuration" switch
-    sudo nixos-rebuild boot --flake ".#"
+    nix build -o tmp-system-build ".#nixosConfigurations.$(hostname).config.system.build.toplevel"
+
+    profile="/nix/var/nix/profiles/system"
+    pathToConfig="$(readlink -f tmp-system-build)"
+    rm tmp-system-build
+
+    sudo nix-env -p "$profile" --set "$pathToConfig"
+    sudo "$pathToConfig/specialisation/$1/bin/switch-to-configuration" switch
+    sudo "$pathToConfig/bin/switch-to-configuration" boot
 else
     echo "No spec set, will activate default"
     sudo nixos-rebuild switch --flake ".#"
@@ -14,4 +21,4 @@ fi
 
 # home-manager switch used to handle this?
 # since swapping to home-manager.nixosModules.home-manager seem to need to do this to get changes to happen immediately
-sudo systemctl restart home-manager-"$USER"
+sudo systemctl restart home-manager-\* --all
