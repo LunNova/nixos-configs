039bdc5bcfecdaea0458a78a71b46737b4a723ef
Luna Nova <git@lunnova.dev>
2022-01-02 19:03:28 -0800

    Reduce indentation level for config.fileSystems for hisame

--- hosts/hisame/default.nix
+++ hosts/hisame/default.nix
@@ -1,5 +1,8 @@
 { config, pkgs, lib, nixos-hardware-modules-path, ... }:
-let name = "hisame"; in
+let
+  name = "hisame";
+  swap = "/dev/disk/by-partlabel/_swap";
+in
 {
   imports = [
     "${nixos-hardware-modules-path}/common/gpu/amd"
@@ -17,56 +20,54 @@ let name = "hisame"; in
     users.mutableUsers = false;
 
     lun.persistence.enable = true;
-    fileSystems =
-      let swap = "/dev/disk/by-partlabel/_swap"; in
-      {
-        "/" = {
-          device = "tmpfs";
-          fsType = "tmpfs";
-          options = [
-            "defaults"
-            "size=2G"
-            "mode=755"
-          ];
-        };
-        "/boot" = {
-          device = "/dev/disk/by-partlabel/_esp";
-          fsType = "vfat";
-          neededForBoot = true;
-          options = [ "discard" "noatime" ];
-        };
-        "/persist" = {
-          device = "/dev/disk/by-partlabel/${name}_persist";
-          fsType = "ext4";
-          neededForBoot = true;
-          options = [ "discard" "noatime" ];
-        };
-        "/nix" = {
-          device = "/persist/nix";
-          noCheck = true;
-          fsType = "none";
-          neededForBoot = true;
-          options = [ "bind" ];
-        };
-        "/home" = {
-          device = "/persist/home";
-          noCheck = true;
-          neededForBoot = true;
-          options = [ "bind" ];
-        };
-        "/var/log" = {
-          device = "/persist/var/log";
-          noCheck = true;
-          neededForBoot = true;
-          options = [ "bind" ];
-        };
-        "/tmp" = {
-          fsType = "tmpfs";
-          device = "tmpfs";
-          neededForBoot = true;
-          options = [ "mode=1777" "rw" "nosuid" "nodev" "size=32G" ];
-        };
+    fileSystems = {
+      "/" = {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [
+          "defaults"
+          "size=2G"
+          "mode=755"
+        ];
       };
+      "/boot" = {
+        device = "/dev/disk/by-partlabel/_esp";
+        fsType = "vfat";
+        neededForBoot = true;
+        options = [ "discard" "noatime" ];
+      };
+      "/persist" = {
+        device = "/dev/disk/by-partlabel/${name}_persist";
+        fsType = "ext4";
+        neededForBoot = true;
+        options = [ "discard" "noatime" ];
+      };
+      "/nix" = {
+        device = "/persist/nix";
+        noCheck = true;
+        fsType = "none";
+        neededForBoot = true;
+        options = [ "bind" ];
+      };
+      "/home" = {
+        device = "/persist/home";
+        noCheck = true;
+        neededForBoot = true;
+        options = [ "bind" ];
+      };
+      "/var/log" = {
+        device = "/persist/var/log";
+        noCheck = true;
+        neededForBoot = true;
+        options = [ "bind" ];
+      };
+      "/tmp" = {
+        fsType = "tmpfs";
+        device = "tmpfs";
+        neededForBoot = true;
+        options = [ "mode=1777" "rw" "nosuid" "nodev" "size=32G" ];
+      };
+    };
     swapDevices = [{
       device = swap;
     }];
