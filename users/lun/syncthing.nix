c0861a383f25b12c69754138de9cf5c6cd2e93d2
Luna Nova <git@lunnova.dev>
2022-01-06 07:56:35 -0800

    Delay syncthingtray start

--- users/lun/syncthing.nix
+++ users/lun/syncthing.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   services.syncthing = {
     enable = true;
     tray.enable = true;
+  };
+
+  systemd.user.services.${config.services.syncthing.tray.package.pname} = {
+    Install.WantedBy = lib.mkForce [ ];
+  };
+  systemd.user.timers.${config.services.syncthing.tray.package.pname} = {
+    Timer = {
+      OnActiveSec = "10s";
+      AccuracySec = "1s";
+    };
+    Install = { WantedBy = [ "graphical-session.target" ]; };
   };
 }
