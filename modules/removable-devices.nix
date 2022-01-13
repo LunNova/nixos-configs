ecd931efa03f12e34d50534643cafd0ae9d0980e
Luna Nova <git@lunnova.dev>
2022-01-12 20:48:52 -0800

    Disable suspend for RTL8156

--- modules/removable-devices.nix
+++ modules/removable-devices.nix
@@ -5,8 +5,10 @@
       matchConfig.PermanentMACAddress = "8c:ae:4c:dd:20:d8";
       linkConfig.Name = "en-usb-8cd8";
     };
+
+    # Disable suspend for USB network RTL8156
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", TEST=="power/control", ATTR{power/control}="on"
+    '';
   };
 }
-
-  
-
