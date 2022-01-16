e3f7f1a9bb84b56c134ae40cad7e0e676c18994e
Luna Nova <git@lunnova.dev>
2022-01-16 09:57:40 -0800

    cleanup-old-generations now checks for root and nixos, and does an fstrim

--- scripts/cleanup-old-generations.sh
+++ scripts/cleanup-old-generations.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+if [[ ! -f /etc/NIXOS ]]; then echo "This script is only for nixos systems"; exit 1; fi
+if [[ $(id -u) -ne 0 ]]; then echo "Please run as root"; exit 1; fi
+
 set -xeuo pipefail
 
-sudo nix-collect-garbage --delete-older-than 3d
-sudo /nix/var/nix/profiles/system/bin/switch-to-configuration boot
+nix-collect-garbage --delete-older-than 3d
+/nix/var/nix/profiles/system/bin/switch-to-configuration boot
 
 # TODO autooptimise?
 nix-store --optimise
+
+fstrim -av
