746fef0328b0c6479cde7be3bf47576ea3a3806b
Luna Nova <git@lunnova.dev>
2022-02-02 22:52:43 -0800

    Set discord flags to force accel and no smooth scrolling

--- users/lun/discord.nix
+++ users/lun/discord.nix
@@ -1,13 +1,12 @@
 { config, lib, pkgs, inputs, self, ... }:
 {
   home.packages = [
-    (pkgs.discord-plugged.override {
-      # TODO make it launch with 
-      # discordcanary --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
-      discord-canary = pkgs.discord-canary.override { };
-      powercord = pkgs.lun.powercord;
-      plugins = pkgs.powercord-plugins;
-      themes = pkgs.powercord-themes;
+    (pkgs.lun.discord-plugged.override {
+      extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
+      powercord = pkgs.lun.powercord.override {
+        plugins = pkgs.powercord-plugins;
+        themes = pkgs.powercord-themes;
+      };
     })
   ];
 }
