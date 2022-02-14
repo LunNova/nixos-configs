536b648c9ffac11bfe493185c6a4068c2e20da5b
Luna Nova <git@lunnova.dev>
2022-02-13 17:19:34 -0800

    Update discord-canary to 133

--- modules/temp-upgrade-overlays.nix
+++ modules/temp-upgrade-overlays.nix
@@ -1,17 +1,7 @@
 { pkgs, ... }: {
   config = {
     nixpkgs.overlays = [
-      (final: prev: {
-        # https://github.com/NixOS/nixpkgs/pull/154698
-        discord-canary = let version = "0.0.132"; in
-          prev.discord-canary.overrideAttrs (prev: {
-            inherit version;
-            src = builtins.fetchurl {
-              url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-              sha256 = "1jjbd9qllgcdpnfxg5alxpwl050vzg13rh17n638wha0vv4mjhyv";
-            };
-          });
-      })
+
     ];
   };
 }
