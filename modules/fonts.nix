a3bf6c1a2f838b629ff07d834968ba74d91c3704
Luna Nova <git@lunnova.dev>
2022-02-17 08:34:24 -0800

    Workaround low quality lucida x font issue
    See https://discourse.nixos.org/t/cant-make-lucida-font-work-with-home-manager/14143
    and https://github.com/NixOS/nixpkgs/issues/41154

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
   fonts = {
-    enableDefaultFonts = true;
-    fonts = with pkgs; [
+    enableDefaultFonts = false;
+
+    # Use mkForce because we can't turn off the x fonts while x server is enabled
+    fonts = with pkgs; lib.mkForce [
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
@@ -15,6 +17,23 @@
       font-awesome_4
       font-awesome_5
       (nerdfonts.override { fonts = [ "Hack" ]; })
+
+      # Defaults from https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/config/fonts/fonts.nix
+      dejavu_fonts
+      freefont_ttf
+      gyre-fonts # TrueType substitutes for standard PostScript fonts
+      liberation_ttf
+      unifont
+      noto-fonts-emoji
     ];
+
+    # You also can't override the lucida font packages
+    # not possible due to https://github.com/NixOS/nixpkgs/issues/41154
+    # xorg = prev.xorg.overrideScope' (self: super:
+    #   {
+    #     fontbhlucidatypewriter100dpi = final.emptyDirectory;
+    #     fontbhlucidatypewriter75dpi = final.emptyDirectory;
+    #   }
+    # );
   };
 }
