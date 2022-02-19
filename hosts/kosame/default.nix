23f0addca680f1b0c7f8f95c31c368a822c24e7d
Luna Nova <git@lunnova.dev>
2022-02-19 13:55:31 -0800

    Tweak asus battery charge limit settings for kosame

--- hosts/kosame/default.nix
+++ hosts/kosame/default.nix
@@ -63,10 +63,11 @@ in
     after = [ "multi-user.target" "suspend.target" ];
     description = "Set the battery charge threshold";
     startLimitBurst = 5;
+    startLimitIntervalSec = 1;
     serviceConfig = {
       Type = "oneshot";
       Restart = "on-failure";
-      ExecStart = "/bin/sh -c 'echo 70 > /sys/class/power_supply/BAT0/charge_control_end_threshold'";
+      ExecStart = "/bin/sh -c 'echo 65 > /sys/class/power_supply/BAT0/charge_control_end_threshold'";
     };
   };
 
