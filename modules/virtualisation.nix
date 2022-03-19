1c2a559d629742e7f44011edc752dd72a7518e0f
Luna Nova <git@lunnova.dev>
2022-03-19 15:25:11 -0700

    virtualisation: add libvirtd and virtmanager

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,6 +1,22 @@
+{ pkgs, ... }:
 {
   config = {
     virtualisation.docker.enable = true;
     lun.persistence.dirs = [ "/var/lib/docker" ];
+
+    environment.systemPackages = with pkgs; [
+      virtmanager
+      virtiofsd
+    ];
+
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        ovmf.enable = true;
+        runAsRoot = false;
+      };
+      onBoot = "ignore";
+      onShutdown = "shutdown";
+    };
   };
 }
