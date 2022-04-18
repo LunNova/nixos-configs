cd33e39867a08791937330ffbaaac78bb4a196a6
Luna Nova <git@lunnova.dev>
2022-04-18 08:40:59 -0700

    nix-binary-caches: Set priorities, use cache.nixos.org > nix-community.cachix > garnix

--- modules/nix-binary-caches.nix
+++ modules/nix-binary-caches.nix
@@ -1,14 +1,14 @@
 {
   config.nix.settings = {
     substituters = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org/"
-      "https://lun-nixos-configs.cachix.org/"
+      "https://cache.nixos.org/?priority=10"
+      "https://cache.garnix.io/?priority=9999"
+      "https://nix-community.cachix.org/?priority=20"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "lun-nixos-configs.cachix.org-1:4jBzwzEn7vbyrnTMz9lc5JF8fYBTIT6gWDiutrkxmDU="
     ];
   };
 }
