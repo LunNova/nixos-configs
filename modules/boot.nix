06d5074a44863f0245f24c49f140f27b67bc8813
Luna Nova <git@lunnova.dev>
2022-04-18 08:43:30 -0700

    boot: document trying to use initrd.systemd.enable

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,6 +2,9 @@
 {
   config = {
     boot = {
+      # systemd in stage-1 initrd is cool but it doesn't work yet on at least kosame
+      # TODO: Check if it works now, and bug report if it still doesn't
+      # initrd.systemd.enable = true;
       kernelPackages = pkgs.linuxPackages_latest;
       loader = {
         systemd-boot.enable = true;
