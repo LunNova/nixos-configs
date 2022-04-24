fb8c10b0866633c67d216af9227d208f133b8180
Luna Nova <git@lunnova.dev>
2022-04-24 13:23:34 -0700

    scripts: remove scripts/apply-system and replace it with apply-flake

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
 cd "$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
-sudo nixos-rebuild switch --flake .#
+
+echo "Activating ${1:-toplevel}"
+nix build -o tmp-system-build ".#nixosConfigurations.$(hostname).config.system.build.toplevel"
+
+profile="/nix/var/nix/profiles/system"
+pathToConfig="$(readlink -f tmp-system-build)"
+rm tmp-system-build
+
+sudo nix-env -p "$profile" --set "$pathToConfig"
+
+if [[ -v 1 ]]; then
+  sudo "$pathToConfig/specialisation/$1/bin/switch-to-configuration" switch
+else
+  sudo "$pathToConfig/bin/switch-to-configuration" switch
+fi
+sudo "$pathToConfig/bin/switch-to-configuration" boot
+
+# home-manager switch used to handle this?
+# since swapping to home-manager.nixosModules.home-manager seem to need to do this to get changes to happen immediately
+sudo systemctl restart home-manager-\* --all
