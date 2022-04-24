4f8cd98af25c7a1cd20d62af602c393d3ce66a65
Luna Nova <git@lunnova.dev>
2022-04-24 13:32:25 -0700

    xdg-open-workaround: fix override to use postInstall

--- modules/exported/xdg-open-workaround.nix
+++ modules/exported/xdg-open-workaround.nix
@@ -4,7 +4,7 @@
     nixpkgs.overlays = [
       (self: super: {
         xdg-utils = super.xdg-utils.overrideAttrs (old: {
-          postInstall = (old.postBuild or "") + ''
+          postInstall = (old.postInstall or "") + ''
             
             sed -i '2i if ${pkgs.lun.xdg-open-with-portal}/bin/xdg-open "$1"; then exit 0; fi;' $out/bin/xdg-open
           '';
