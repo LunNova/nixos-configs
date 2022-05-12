993a2de5d4542fd00d9b39aee03205e77abdf338
Luna Nova <git@lunnova.dev>
2022-05-12 00:10:42 -0700

    lun,packages: move discord package with plugins to packages output

--- packages/default.nix
+++ packages/default.nix
@@ -20,23 +20,31 @@ let
   };
   lun-scripts = wrapScripts ./lun-scripts;
   lun-scripts-path = pkgs.symlinkJoin { name = "lun-scripts"; paths = lib.attrValues lun-scripts; };
+  self = {
+    discord-plugged = pkgs.callPackage ./discord-plugged {
+      inherit powercord;
+      inherit (flake-args) powercord-overlay;
+      inherit (pkgs) discord-canary;
+    };
+    inherit powercord xdg-open-with-portal;
+    discord-plugged-lun = self.discord-plugged.override {
+      extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
+      powercord = pkgs.lun.powercord.override {
+        plugins = pkgs.powercord-plugins;
+        themes = pkgs.powercord-themes;
+      };
+    };
+    kwinft = pkgs.lib.recurseIntoAttrs (pkgs.callPackage ./kwinft { });
+    lutris = pkgs.lutris.override {
+      inherit lutris-unwrapped;
+      # extraLibraries = pkgs: [ (pkgs.hiPrio xdg-open-with-portal) ];
+    };
+    spawn = pkgs.callPackage ./spawn { };
+    swaysome = pkgs.callPackage ./swaysome { };
+    sworkstyle = pkgs.callPackage ./sworkstyle { };
+    lun = pkgs.writeShellScriptBin "lun" ''
+      exec "${lun-scripts-path}/bin/$1" "''${@:1}"
+    '';
+  };
 in
-{
-  inherit powercord xdg-open-with-portal;
-  discord-plugged = pkgs.callPackage ./discord-plugged {
-    inherit powercord;
-    inherit (flake-args) powercord-overlay;
-    inherit (pkgs) discord-canary;
-  };
-  kwinft = pkgs.lib.recurseIntoAttrs (pkgs.callPackage ./kwinft { });
-  lutris = pkgs.lutris.override {
-    inherit lutris-unwrapped;
-    # extraLibraries = pkgs: [ (pkgs.hiPrio xdg-open-with-portal) ];
-  };
-  spawn = pkgs.callPackage ./spawn { };
-  swaysome = pkgs.callPackage ./swaysome { };
-  sworkstyle = pkgs.callPackage ./sworkstyle { };
-  lun = pkgs.writeShellScriptBin "lun" ''
-    exec "${lun-scripts-path}/bin/$1" "''${@:1}"
-  '';
-}
+self
--- users/lun/discord.nix
+++ users/lun/discord.nix
@@ -1,12 +1,6 @@
 { config, lib, pkgs, inputs, self, ... }:
 {
   home.packages = [
-    (pkgs.lun.discord-plugged.override {
-      extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
-      powercord = pkgs.lun.powercord.override {
-        plugins = pkgs.powercord-plugins;
-        themes = pkgs.powercord-themes;
-      };
-    })
+    pkgs.lun.discord-plugged-lun
   ];
 }
