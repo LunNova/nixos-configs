46be9e51bed216037a86b1213f01ca31d64f7e9c
Luna Nova <git@lunnova.dev>
2022-05-12 01:43:09 -0700

    discord-plugged: handle both binary and old wrappers
    See https://github.com/NixOS/nixpkgs/issues/172592

--- packages/discord-plugged/default.nix
+++ packages/discord-plugged/default.nix
@@ -4,7 +4,11 @@
 , powercord
 , extraElectronArgs ? ""
 , makeBinaryWrapper
+, writeShellScript
 }:
+let extractCmd = makeBinaryWrapper.extractCmd or writeShellScript "extract-binary-wrapper-cmd" ''
+  strings -dw "$1" | sed -n '/^makeCWrapper/,/^$/ p'
+''; in
 symlinkJoin {
   name = "discord-plugged";
   paths = [ discord-canary.out ];
@@ -20,7 +24,29 @@ symlinkJoin {
     cp -r --remove-destination ${powercord-overlay}/plugs/* $injectTarg/
     substituteInPlace $injectTarg/index.js --replace 'POWERCORD_SRC' '${powercord}'
 
-    makeWrapper $(readlink "$out/opt/DiscordCanary/DiscordCanary") $out/opt/DiscordCanary/DiscordCanary --add-flags "${extraElectronArgs}"
+    cp -a --remove-destination $(readlink "$out/opt/DiscordCanary/.DiscordCanary-wrapped") "$out/opt/DiscordCanary/.DiscordCanary-wrapped" || true
+    cp -a --remove-destination $(readlink "$out/opt/DiscordCanary/DiscordCanary") "$out/opt/DiscordCanary/DiscordCanary"
+
+    if grep '\0' $out/opt/DiscordCanary/DiscordCanary && wrapperCmd=$(${extractCmd} $out/opt/DiscordCanary/DiscordCanary) && [[ $wrapperCmd ]]; then
+      # Binary wrapper
+      parseMakeCWrapperCall() {
+        shift # makeCWrapper
+        oldExe=$1; shift
+        oldWrapperArgs=("$@")
+      }
+      eval "parseMakeCWrapperCall ''${wrapperCmd//"${discord-canary.out}"/"$out"}"
+      # Binary wrapper
+      makeWrapper $oldExe $out/opt/DiscordCanary/DiscordCanary "''${oldWrapperArgs[@]}" --add-flags "${extraElectronArgs}"
+    else
+      # Normal wrapper
+      substituteInPlace $out/opt/DiscordCanary/DiscordCanary \
+      --replace '${discord-canary.out}' "$out" \
+      --replace '"$@"' '${extraElectronArgs} "$@"'
+    fi
+
+    rm $out/bin/discordcanary $out/bin/DiscordCanary || true
+    ln -s $out/opt/DiscordCanary/DiscordCanary $out/bin/discordcanary
+    ln -s $out/opt/DiscordCanary/DiscordCanary $out/bin/DiscordCanary
   '';
 
   meta.mainProgram = if (discord-canary.meta ? mainProgram) then discord-canary.meta.mainProgram else null;
