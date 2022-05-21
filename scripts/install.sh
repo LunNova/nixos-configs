2b83dcf6e1a73bc16a694aa62fd11537dff61dca
Luna Nova <git@lunnova.dev>
2022-05-21 16:52:52 -0700

    scripts/install.sh: extract from README.md

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+BOOT_PARTITION=/dev/disk/by-partlabel/_esp
+PERSIST_PARTITION=/dev/disk/by-partlabel/hostname_persist
+FLAKE=github:TransLunarInjection/nixos-configs/dev#hostname
+
+mount -t tmpfs none /mnt
+
+mkdir -p /mnt/{boot,persist}
+
+mount $BOOT_PARTITION /mnt/boot
+mount $PERSIST_PARTITION /mnt/persist
+
+mkdir -p /mnt/{boot,persist,home,nix,var/log} /mnt/persist/{home,nix,var/log}
+
+mount -o bind /mnt/persist/nix /mnt/nix
+mount -o bind /mnt/persist/home /mnt/home
+mount -o bind /mnt/persist/var/log /mnt/var/log
+
+# Install nixos:
+nix-shell -p git nixFlakes --run "nixos-install --impure --no-root-passwd --root /mnt --flake $FLAKE"
+# Install refind efi boot manager
+nix-shell -p efibootmgr refind --run "refind-install --root /mnt"
