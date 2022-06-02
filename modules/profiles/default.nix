f374dd067899431125873c1150df4cf059b675fd
Luna Nova <git@lunnova.dev>
2022-06-01 18:42:17 -0700

    profiles/personal: init with persistence config

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -1,9 +1,11 @@
 { ... }:
 {
+  # TODO: make these optin per host
   imports = [
     ./common.nix
     ./graphical.nix
     ./gaming.nix
     ./server.nix
+    ./personal.nix
   ];
 }
---
+++ modules/profiles/personal.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  config = {
+    lun.persistence.dirs = [
+      "/root" # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=962987 >:(
+      "/home"
+    ];
+  };
+}
