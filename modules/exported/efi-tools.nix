5c2a895aac5bfc7bcc92cbb00af1a82638002272
Luna Nova <git@lunnova.dev>
2022-06-01 18:41:00 -0700

    modules/efi-tools: init

---
+++ modules/exported/efi-tools.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.lun.efi-tools;
+  inherit (config.boot.loader) efi;
+  copyTool = source: dest_filename:
+    ''
+      if [ -d "${efi.efiSysMountPoint}/EFI" ] && [ -e "${source}" ]; then
+        in_package="${source}"
+        in_esp="${efi.efiSysMountPoint}/EFI/tools/${dest_filename}"
+        >&2 echo "Ensuring ${dest_filename} in EFI System Partition"
+        if ! ${pkgs.diffutils}/bin/cmp --silent "$in_package" "$in_esp"; then
+          >&2 echo "Copying $in_package -> $in_esp"
+          cp "$in_package" "$in_esp"
+          sync
+        fi
+      else
+        >&2 echo "Skipping ${source} as EFI directory or source .efi missing"
+      fi
+    '';
+in
+{
+  options.lun.efi-tools = {
+    enable = lib.mkEnableOption "Enable copying tools to EFI System Partition tools directory";
+    tools = lib.mkOption {
+      default = { };
+      type = lib.types.attrsOf (lib.types.nullOr lib.types.path);
+      description = "Set of .efi files";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    lun.efi-tools.tools = {
+      memtest86 = lib.mkDefault pkgs.lun.memtest86plus.efi;
+      shell = lib.mkDefault pkgs.lun.uefi-shell.efi;
+    };
+    system.activationScripts.copy-efi-tools = lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: copyTool value (name + ".efi")) cfg.tools);
+  };
+}
