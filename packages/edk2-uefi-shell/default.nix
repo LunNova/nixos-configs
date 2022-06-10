8ac6f3d43ef425776bfe0289b1c730a78fc25140
Luna Nova <git@lunnova.dev>
2022-06-09 17:35:24 -0700

    edk2-uefi-shell: Add meta attrs, improve shell install

--- packages/edk2-uefi-shell/default.nix
+++ packages/edk2-uefi-shell/default.nix
@@ -1,4 +1,5 @@
-{ edk2
+{ lib
+, edk2
 , util-linux
 , nasm
 , python3
@@ -7,18 +8,27 @@
 (edk2.mkDerivation "ShellPkg/ShellPkg.dsc" {
   pname = "edk2-uefi-shell";
   inherit (edk2) version;
+
   nativeBuildInputs = [ util-linux nasm python3 ];
-  noAuditTmpdir = true;
   strictDeps = true;
+
+  # We only have a .efi file in $out which shouldn't be patched or stripped
   dontPatchELF = true;
   dontStrip = true;
-  FULL_SHELL_GUID = "EA4BB293-2D7F-4456-A681-1F22F42CD0BC";
+
+  # GUID hardcoded to match ShellPkg.dsc
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/
-    cp Build/Shell/RELEASE*/*/Shell_"$FULL_SHELL_GUID".efi $out/shell.efi
+    install -D -m0644 Build/Shell/RELEASE*/*/Shell_EA4BB293-2D7F-4456-A681-1F22F42CD0BC.efi $out/shell.efi
     runHook postInstall
   '';
+
+  meta = {
+    inherit (edk2.meta) license platforms;
+    description = "UEFI Shell from Tianocore EFI development kit";
+    homepage = "https://github.com/tianocore/tianocore.github.io/wiki/ShellPkg";
+    maintainers = with lib.maintainers; [ LunNova ];
+  };
 }).overrideAttrs (finalAttrs: previousAttrs: {
   passthru.efi = "${finalAttrs.finalPackage}/shell.efi";
 })
