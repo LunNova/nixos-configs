03401dd5b1c768cdfe31054412bd60a6a9e20e80
Luna Nova <git@lunnova.dev>
2022-06-11 19:51:18 -0700

    modules/amd-mem-encrypt: init

---
+++ modules/exported/amd-mem-encrypt.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.lun.amd-mem-encrypt;
+in
+{
+  options.lun.amd-mem-encrypt = {
+    enable = lib.mkEnableOption "Enable AMD memory encryption";
+  };
+  config = lib.mkIf cfg.enable {
+    boot = {
+      kernelParams = [
+        "kvm_amd.sev=1"
+        "mem_encrypt=on"
+      ];
+      kernelPatches = [
+        {
+          name = "enable-amd-sme-sev";
+          patch = null;
+          extraStructuredConfig = with lib.kernel; {
+            AMD_MEM_ENCRYPT = yes;
+            AMD_MEM_ENCRYPT_ACTIVE_BY_DEFAULT = yes;
+          };
+        }
+      ];
+    };
+  };
+}
