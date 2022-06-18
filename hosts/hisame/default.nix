bc652570f2b1241978943113189e5883c5da7cec
Luna Nova <git@lunnova.dev>
2022-06-18 14:52:19 -0700

    hisame: set lun.efi-tools.enable

--- hosts/hisame/default.nix
+++ hosts/hisame/default.nix
@@ -21,6 +21,8 @@ in
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
     lun.amd-mem-encrypt.enable = true;
 
+    lun.efi-tools.enable = true;
+
     boot.initrd.kernelModules = [ "amdgpu" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
     lun.ml = {
