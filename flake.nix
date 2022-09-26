6fe9bf7b1a50437f2e7922086a9e77e142fd1f19
Luna Nova <git@lunnova.dev>
2022-09-26 05:14:45 -0700

    flake: update nixpkgs-stable to 22.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-21.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-22.05";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
