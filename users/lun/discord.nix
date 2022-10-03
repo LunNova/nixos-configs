d653d7ec86fea2ba9c1054e0059e77e96987f0bd
Luna Nova <git@lunnova.dev>
2022-10-02 18:24:26 -0700

    discord: enable withOpenAsar

--- users/lun/discord.nix
+++ users/lun/discord.nix
@@ -3,6 +3,7 @@
   home.packages = [
     (flake-args.replugged-nix-flake.lib.makeDiscordPlugged {
       inherit pkgs;
+      withOpenAsar = true;
       themes = pkgs.powercord-themes;
       plugins = pkgs.powercord-plugins;
       extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
