5dd9e3438d7379d67ae17b75c7b51580e7215839
Luna Nova <git@lunnova.dev>
2022-10-13 08:48:12 -0700

    boot: use mkDefault when setting latest kernel

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,11 @@
-{ lun, pkgs, ... }:
+{ lun, pkgs, lib, ... }:
 {
   config = {
     boot = {
       # systemd in stage-1 initrd is cool but it doesn't work yet on at least kosame
       # TODO: Check if it works now, and bug report if it still doesn't
       # initrd.systemd.enable = true;
-      kernelPackages = pkgs.linuxPackages_latest;
+      kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
       loader = {
         systemd-boot.enable = true;
         efi.canTouchEfiVariables = false;
