7451d14eb1b36b9302be6bd00e7865f51c1a0a86
Luna Nova <git@lunnova.dev>
2022-10-19 08:58:26 -0700

    lun/gaming: remove polymc pending prismlauncher release
    see https://github.com/NixOS/nixpkgs/issues/196480

--- users/lun/gaming.nix
+++ users/lun/gaming.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     # osu-lazer not currently playing
-    polymc # Hi emstar (:
+    # FIXME: replace with prismlauncher polymc # Hi emstar (:
     lun.lutris
     # TODO: try bottles instead of lutris
   ];
