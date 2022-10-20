cef55e307cd6d328e313beb0bd1ff4ee4c17d84b
Luna Nova <git@lunnova.dev>
2022-10-19 21:12:24 -0700

    lun/gaming: add prismlauncher

--- users/lun/gaming.nix
+++ users/lun/gaming.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     # osu-lazer not currently playing
-    # FIXME: replace with prismlauncher polymc # Hi emstar (:
+    prismlauncher # Hi emstar (:
     lun.lutris
     # TODO: try bottles instead of lutris
   ];
