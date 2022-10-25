39d6752080575d28a69256e3651e9e46c7518341
Luna Nova <git@lunnova.dev>
2022-10-25 08:49:46 -0700

    amd-pstate: add sharedMem option which sets amd_pstate.shared_mem=1

--- modules/exported/amd-pstate.nix
+++ modules/exported/amd-pstate.nix
@@ -5,6 +5,7 @@ in
 {
   options.lun.amd-pstate = {
     enable = lib.mkEnableOption "Enable AMD memory encryption";
+    sharedMem = lib.mkEnableOption "use shared_mem";
   };
   config = lib.mkIf cfg.enable {
     # If won't load try sudo modprobe amd_pstate dyndbg==pmf shared_mem=1 -v
@@ -14,9 +15,11 @@ in
     # amd_pstate:amd_pstate_init: amd_pstate: the _CPC object is not present in SBIOS
     # option can be found here: Advanced > AMD CBS > NBIOS Common Options > SMU Common Options > CPPC > CPPC CTRL set to Enabled
     boot = {
-      kernelParams = [
-        "initcall_blacklist=acpi_cpufreq_init" # use amd_pstate instead
-        #"amd_pstate.shared_mem=1"
+      kernelParams = lib.mkMerge [
+        [
+          "initcall_blacklist=acpi_cpufreq_init" # use amd_pstate instead
+        ]
+        (lib.mkIf cfg.sharedMem [ "amd_pstate.shared_mem=1" ])
       ];
       initrd.kernelModules = [ "amd_pstate" ];
       # kernelPatches = [
