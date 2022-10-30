ff1aa06bde6e108fdd8a469c3ef8541b629e0fb0
Luna Nova <git@lunnova.dev>
2022-10-30 11:38:20 -0700

    partition-scratch-drive.sh: make swap optional and use for bigscratch

--- scripts/install/partition-scratch-drive.sh
+++ scripts/install/partition-scratch-drive.sh
@@ -6,10 +6,13 @@
 set -euo pipefail
 
 # Examples, update before use
+type="bigscratch"
 prefix="hisame_"
 suffix=""
-swap_size="256GiB"
-device=/dev/nvme0n1
+swap_size="0"
+#swap_size="256GiB"
+device=/dev/sdc
+partprefix=""
 
 lsblk --output "NAME,SIZE,FSTYPE,FSVER,LABEL,PARTLABEL,UUID,FSAVAIL,FSUSE%,MOUNTPOINTS,DISC-MAX" "$device"
 
@@ -22,26 +25,36 @@ blkdiscard -f -s "$device" || blkdiscard -f "$device"
 sync
 sleep 1
 
-parted -s -a optimal -- "$device" \
-	mklabel gpt \
-	mkpart "${prefix}swap${suffix}" linux-swap 1MiB "$swap_size" \
-	set 1 esp on \
-	mkpart "${prefix}scratch${suffix}" btrfs "$swap_size" 100%
+if [ "z$swap_size" = "z0" ]; then
+	echo "not making swap part"
+	datapart="${device}${partprefix}1"
+	parted -s -a optimal -- "$device" \
+		mklabel gpt \
+		mkpart "${prefix}${type}${suffix}" btrfs 1MiB 100%
+else
+	datapart="${device}${partprefix}2"
+	parted -s -a optimal -- "$device" \
+		mklabel gpt \
+		mkpart "${prefix}swap${suffix}" linux-swap 1MiB "$swap_size" \
+		mkpart "${prefix}${type}${suffix}" btrfs "$swap_size" 100%
+fi
 
 sync
 sleep 1
 
-mkswap /dev/disk/by-partlabel/"${prefix}swap${suffix}" -L _swap
-mkfs.btrfs -f "${device}p2" -R free-space-tree -L _scratch
+if [ "z$swap_size" != "z0" ]; then
+	mkswap /dev/disk/by-partlabel/"${prefix}swap${suffix}" -L _swap
+fi
+mkfs.btrfs -f "${datapart}" -R free-space-tree -L _${type}
 
 sync
 sleep 1
 
 mountdir="$(mktemp -d)"
 
-mount -t btrfs -o defaults,ssd,nosuid,nodev,compress=zstd,noatime "${device}p2" "$mountdir"
+mount -t btrfs -o defaults,ssd,nosuid,nodev,compress=zstd,noatime "${datapart}" "$mountdir"
 
-btrfs subvolume create "$mountdir"/@scratch
+btrfs subvolume create "$mountdir"/@main
 btrfs subvolume sync "$mountdir"
 
 sleep 1
