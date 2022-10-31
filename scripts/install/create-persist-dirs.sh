7425f6e9e03030d2e2e31d84fbc75c9c53be982a
Luna Nova <git@lunnova.dev>
2022-10-30 19:48:35 -0700

    create-persist-dirs.sh: remove /mnt prefix

--- scripts/install/create-persist-dirs.sh
+++ scripts/install/create-persist-dirs.sh
@@ -3,7 +3,7 @@ set -xeuo pipefail
 IFS="$(printf "\n")"
 
 for dir in $(nix eval --raw ".#nixosConfigurations.$(hostname).config.lun.persistence.dirs_for_shell_script"); do
-	mkdir "/mnt$dir"
-	mkdir "/mnt/persist$dir"
-	mount -o bind "/mnt/persist$dir" "/mnt$dir"
+	mkdir "$dir"
+	mkdir "/persist$dir"
+	mount -o bind "/persist$dir" "$dir"
 done
