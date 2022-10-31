e01b18c48af61b33e5a8119cec519c275a33f915
Luna Nova <git@lunnova.dev>
2022-10-30 18:56:20 -0700

    server: disable hardware.opengl

--- modules/profiles/server.nix
+++ modules/profiles/server.nix
@@ -3,5 +3,8 @@
   options.lun.profiles.server = lib.mkEnableOption "Enable server profile";
   config = lib.mkIf config.lun.profiles.server {
     sound.enable = false;
+    hardware.opengl = lib.mkForce {
+      enable = false;
+    };
   };
 }
