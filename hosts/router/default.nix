94a08a9befe65b85c41f96c77c074bd1d4a57083
Luna Nova <git@lunnova.dev>
2022-10-30 22:24:29 -0700

    router: set network-online.target as wants for more network services

--- hosts/router/default.nix
+++ hosts/router/default.nix
@@ -174,9 +174,10 @@ in
       };
     };
     systemd.targets.network-online.wants = [ "systemd-networkd-wait-online.service" ];
-    systemd.targets.network-online.requires = [ "systemd-networkd-wait-online.service" ];
     systemd.services.dnsmasq.wants = [ "network-online.target" ];
-    systemd.services.dnsmasq.requires = [ "network-online.target" ];
+    systemd.services.avahi.wants = [ "network-online.target" ];
+    systemd.services.miniupnpd.wants = [ "network-online.target" ];
+    systemd.services.lldpd.wants = [ "network-online.target" ];
     # dnsmasq handles dhcp and dns
     services.dnsmasq = {
       enable = true;
