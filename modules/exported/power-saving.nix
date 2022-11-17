11e065b7da15ed007913d6e8901f2e4b5a6be25d
Luna Nova <git@lunnova.dev>
2022-11-16 20:08:55 -0800

    power-saving: separate out usb power saving as it causes too many problems

--- modules/exported/power-saving.nix
+++ modules/exported/power-saving.nix
@@ -5,6 +5,7 @@ in
 {
   options.lun.power-saving = {
     enable = lib.mkEnableOption "Enable power saving configs";
+    usb = lib.mkEnableOption "Enable USB power saving configs (tends to break some peripherals so only use on laptops?)";
   };
   config = lib.mkIf cfg.enable {
     boot.kernelParams = [
@@ -13,61 +14,45 @@ in
       "usbcore.autosuspend=30"
     ];
 
-    /*
-      $ echo manual | sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level
-      manual
-      lun@hisame ~ ./rw
-      $ echo manual | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level
-      manual
-      lun@hisame ~ ./rw
-      $ echo 5 | sudo tee /sys/class/drm/card1/device/pp_power_profile_mode
-      5
-      lun@hisame ~ ./rw
-      $ echo 5 | sudo tee /sys/class/drm/card0/device/pp_power_profile_mode
-      5
-      lun@hisame ~ ./rw
-      $ cat /sys/class/drm/card1/device/pp_power_profile_mode
-    */
+    services.udev.extraRules = lib.mkMerge [
+      ''
+        # enable power control for all PCI devices
+        SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"
+        SUBSYSTEM=="pci", TEST=="d3cold_allowed", ATTR{d3cold_allowed}="1"
 
-    services.udev.extraRules = ''
-      # enable power control for all PCI devices
-      SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"
-      SUBSYSTEM=="pci", TEST=="d3cold_allowed", ATTR{d3cold_allowed}="1"
-      
-      # amdgpu power saving
-      # SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_force_performance_level}="manual"
-      # SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_state}="battery"
-      # SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/pp_power_profile_mode}="2"
+        # Enable power savings for scsi_host (/dev/s*) drives
+        ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", ATTR{link_power_management_policy}="med_power_with_dipm"
 
-      # enable power control for all USB devices except multifunction, HID and hubs (00, 03, 09)
-      # https://www.usb.org/defined-class-codes
-      SUBSYSTEM=="usb", TEST=="power/control", \
-        ATTR{bInterfaceClass}!="00", ATTR{bInterfaceClass}!="03", ATTR{bInterfaceClass}!="09", \
-        ATTR{bDeviceClass}!="00", ATTR{bDeviceClass}!="03", ATTR{bDeviceClass}!="09", \
-        ATTR{power/control}="auto"
-      SUBSYSTEM=="usb", TEST=="power/control", \
-        ENV{ID_USB_INTERFACES}!="", ENV{ID_USB_INTERFACES}!=":03*", ENV{ID_USB_INTERFACES}!=":09*" \
-        ATTR{bDeviceClass}=="00", \
-        ATTR{power/control}="auto"
+        # power saving for ata devices
+        ACTION=="add", SUBSYSTEM=="ata_port", KERNEL=="ata*", TEST=="../../power/control" ATTR{../../power/control}="auto"
 
-      # Enable power savings for scsi_host (/dev/s*) drives
-      ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", ATTR{link_power_management_policy}="med_power_with_dipm"
+        # misc
+        ATTR{power/async}=="disabled", ATTR{power/async}="enabled"
+        # this is either a on/off switch or a timeout in seconds depending on the module
+        # gets set to 10 later for snd_hda_intel which uses it as seconds timeout
+        TEST=="parameters/power_save", ATTR{parameters/power_save}="1"
+        TEST=="parameters/power_save_controller", ATTR{parameters/power_save_controller}="Y"
 
-      # power saving for ata devices
-      ACTION=="add", SUBSYSTEM=="ata_port", KERNEL=="ata*", TEST=="../../power/control" ATTR{../../power/control}="auto"
+        # wifi power saving
+        ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="${pkgs.iw}/bin/iw dev $name set power_save on"
 
-      # misc
-      SUBSYSTEMS=="*" ATTR{power/async}=="disabled", ATTR{power/async}="enabled"
-      SUBSYSTEMS=="*", TEST=="parameters/power_save", ATTR{parameters/power_save}="1"
-      SUBSYSTEMS=="*", ATTR{parameters/power_save_controller}=="N", ATTR{parameters/power_save_controller}="Y"
-
-      # wifi power saving
-      ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="${pkgs.iw}/bin/iw dev $name set power_save on"
-
-      # Enable power save mode for Intel HDA
-      ACTION=="add", SUBSYSTEM=="module", DEVPATH=="/module/snd_hda_intel", \
-      RUN="${pkgs.bash}/bin/bash -c 'cd /sys/module/snd_hda_intel/parameters; \
-      echo 10 > power_save; echo Y > power_save_controller'"
-    '';
+        # Enable power save mode for Intel HDA
+        ACTION=="add", SUBSYSTEM=="module", DEVPATH=="/module/snd_hda_intel", \
+        RUN="${pkgs.bash}/bin/bash -c 'cd /sys/module/snd_hda_intel/parameters; \
+        echo 10 > power_save; echo Y > power_save_controller'"
+      ''
+      (lib.mkIf cfg.usb ''
+        # enable power control for all USB devices except multifunction, HID and hubs (00, 03, 09)
+        # https://www.usb.org/defined-class-codes
+        SUBSYSTEM=="usb", TEST=="power/control", \
+          ATTR{bInterfaceClass}!="00", ATTR{bInterfaceClass}!="03", ATTR{bInterfaceClass}!="09", \
+          ATTR{bDeviceClass}!="00", ATTR{bDeviceClass}!="03", ATTR{bDeviceClass}!="09", \
+          ATTR{power/control}="auto"
+        SUBSYSTEM=="usb", TEST=="power/control", \
+          ENV{ID_USB_INTERFACES}!="", ENV{ID_USB_INTERFACES}!=":03*", ENV{ID_USB_INTERFACES}!=":09*" \
+          ATTR{bDeviceClass}=="00", \
+          ATTR{power/control}="auto"
+      '')
+    ];
   };
 }
