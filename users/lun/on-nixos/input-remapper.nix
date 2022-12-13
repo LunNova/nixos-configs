4e545d57c01e535f25d59eebf1da506863646f2c
Luna Nova <git@lunnova.dev>
2022-12-12 21:48:50 -0800

    lun/input-remapper: use xdg.configFile

--- users/lun/on-nixos/input-remapper.nix
+++ users/lun/on-nixos/input-remapper.nix
@@ -1,6 +1,6 @@
 { lib, nixosConfig, ... }:
 {
-  home.file.".config/autostart/input-remapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
+  xdg.configFile."autostart/input-mapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
     source = "${nixosConfig.services.input-remapper.package}/share/applications/input-remapper-autoload.desktop";
   };
 }
