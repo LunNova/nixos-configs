67a0520957b80dd552ebaea62d4bac16f961fb2e
Luna Nova <git@lunnova.dev>
2022-12-16 17:45:01 -0800

    boxxy.sh: handle X apps and block /dev/dri

--- packages/lun-scripts/boxxy.sh
+++ packages/lun-scripts/boxxy.sh
@@ -5,8 +5,15 @@ set -xeuo pipefail
 app=$1
 shift
 
+EMPTY="$(mktemp -d)"
 TMPHOME="$HOME/.local/boxxy-$app"
 
 mkdir -p "$TMPHOME"
 
-bwrap --ro-bind /bin /bin --ro-bind /run /run --ro-bind /etc /etc --ro-bind /nix /nix --bind /dev /dev --bind /proc /proc --bind "$TMPHOME" "$HOME" "$@"
+bwrap --ro-bind /bin /bin --ro-bind /run /run \
+	--ro-bind /etc /etc --ro-bind /nix /nix \
+	--dev /dev --bind /proc /proc \
+	--bind "$EMPTY" /dev/dri \
+	--bind "$TMPHOME" "$HOME" \
+	--bind "$HOME/.Xauthority" "$HOME/.Xauthority" \
+	"$@"
