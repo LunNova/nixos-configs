eef9928c88863d5019127614eaae87e9312b93c1
Luna Nova <git@lunnova.dev>
2022-12-16 17:44:35 -0800

    idea-instanced.sh: fix warning due to missing idea.log.path and idea.plugins.path

--- packages/lun-scripts/idea-instanced.sh
+++ packages/lun-scripts/idea-instanced.sh
@@ -6,5 +6,5 @@ instance=${1:-default}
 shift
 path=/home/lun/.config/idea-instances/$instance
 
-(IDEA_PROPERTIES="$(printf "\t-Didea.system.path=%s/sys\t-Didea.config.path=%s/cfg" "$path" "$path")" \
+(IDEA_PROPERTIES="$(printf "\t-Didea.system.path=%s/sys\t-Didea.config.path=%s/cfg\t-Didea.log.path=%s/sys/log\t-Didea.plugins.path=%s/cfg/plugins" "$path" "$path" "$path" "$path")" \
 	idea-ultimate "$@" >/dev/null 2>/dev/null &) &
