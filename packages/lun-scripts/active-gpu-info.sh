46e52fd28cd92c1fbc16d1490284997819af7b70
Luna Nova <git@lunnova.dev>
2022-12-16 21:43:45 -0800

    active-gpu-*.sh: apply formatting fix for new pre-commit-check version

--- packages/lun-scripts/active-gpu-info.sh
+++ packages/lun-scripts/active-gpu-info.sh
@@ -8,12 +8,12 @@ activeBusId=$(printf '%x\n' "$(echo "$vkinfo" | grep -m1 pciBus | expand | tr -s
 for card in /sys/class/drm/card[0-9]; do
 	target=$(readlink -m "$card")
 	if echo "$target" | grep -q ":$activeBusId:00"; then
-		if [ "z${1:-}" != "zquiet" ]; then
+		if [ "z${1-}" != "zquiet" ]; then
 			echo Active GPU is "$activeBusId" which looks like "$target" >&2
 		fi
 		echo "$target"
 	fi
 done
-if [ "z${1:-}" != "zquiet" ]; then
+if [ "z${1-}" != "zquiet" ]; then
 	echo "$driverInfo $deviceName $activeBusId" >&2
 fi
--- packages/lun-scripts/active-gpu-path.sh
+++ packages/lun-scripts/active-gpu-path.sh
@@ -4,7 +4,7 @@ activeBusId=$(printf '%x\n' "$(MESA_VK_DEVICE_SELECT_FORCE_DEFAULT_DEVICE=1 vulk
 for card in /sys/class/drm/card[0-9]; do
 	target=$(readlink -m "$card")
 	if echo "$target" | grep -q ":$activeBusId:00"; then
-		if [ "z${1:-}" != "zquiet" ]; then
+		if [ "z${1-}" != "zquiet" ]; then
 			echo Active GPU is "$activeBusId" which looks like "$target" >&2
 		fi
 		echo "$target"
