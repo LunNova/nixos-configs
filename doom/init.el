7758738f4584fb0154b95506887765fee5d2a49d
Luna Nova <git@lunnova.dev>
2023-02-09 22:15:25 -0800

    doom: enable lua

--- doom/init.el
+++ doom/init.el
@@ -148,7 +148,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        (nix +lsp)               ; I hereby declare "nix geht mehr!"
