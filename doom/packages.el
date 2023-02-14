44b0777abc31ac8d2dd2e7ef5634250cc51bcc4b
Luna Nova <git@lunnova.dev>
2023-02-13 18:12:55 -0800

    doom: pin transient and with-editor due to https://github.com/doomemacs/doomemacs/issues/7078

--- doom/packages.el
+++ doom/packages.el
@@ -90,3 +90,6 @@
   :recipe (:host github
            :repo "andyjda/sticky-shell"
            :files ("sticky-shell.el")))
+
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
