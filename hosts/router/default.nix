4bba81be69f5451ae2a3d3d9aa929918470e3ec0
Luna Nova <git@lunnova.dev>
2023-02-13 18:28:55 -0800

    router: fix quotes in hdparm

--- hosts/router/default.nix
+++ hosts/router/default.nix
@@ -313,7 +313,7 @@ in
 
     # spin down hdds
     powerManagement.powerUpCommands = with pkgs;''
-      ${bash}/bin/bash -c '${hdparm}/bin/hdparm -S 9 -B 127 $(${utillinux}/bin/lsblk -dnp -o name,rota |${gnugrep}/bin/grep \'.*\\s1\'|${coreutils}/bin/cut -d \' \' -f 1)'
+      ${bash}/bin/bash -c "${hdparm}/bin/hdparm -S 9 -B 127 $(${utillinux}/bin/lsblk -dnp -o name,rota |${gnugrep}/bin/grep \'.*\\s1\'|${coreutils}/bin/cut -d \' \' -f 1)"
     '';
 
     boot.kernelModules = [ "tcp_bbr" ];
