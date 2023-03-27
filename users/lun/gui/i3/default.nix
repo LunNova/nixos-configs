4a89046c2c39837c1630150da7ec69de05996cbb
Luna Nova <git@lunnova.dev>
2023-03-27 02:25:16 -0700

    lun/i3: i3status works and bind more stuff

--- users/lun/gui/i3/default.nix
+++ users/lun/gui/i3/default.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
-
+# https://github.com/berbiche/dotfiles/blob/df1cf43f9f7b267e7b4bf3a9e6cc8e4b4defb680/profiles/i3/home-manager/i3-config.nix#L4
 let
   mod = "Mod4";
+  drun = "${pkgs.rofi}/bin/rofi -show run";
+  menu = "${pkgs.rofi}/bin/rofi -show combi";
 in
 {
   xsession.windowManager.i3 = {
@@ -15,7 +17,21 @@ in
       keybindings = config.wayland.windowManager.sway.config.keybindings // {
         "${mod}+x" = "exec sh -c '${pkgs.maim}/bin/maim -s | xclip -selection clipboard -t image/png'";
         "${mod}+q" = "exec sh -c '${pkgs.i3lock}/bin/i3lock -c ba9bff & sleep 5 && xset dpms force of'";
+        "${mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+        "${mod}+space" = "exec ${drun}";
+        "${mod}+d" = "exec ${menu}";
       };
+
+      startup = [
+        {
+          command = "${pkgs.writeShellScript "i3-wallpaper" ''
+            if [ "$XDG_CURRENT_DESKTOP" = "none+i3" ]; then
+              ${pkgs.feh}/bin/feh --bg-scale ~/.background-image
+            fi
+          ''}";
+          notification = false;
+        }
+      ];
 
       bars = [
         {
