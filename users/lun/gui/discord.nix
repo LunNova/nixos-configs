b5165c6c8da836e2723cc41c28f5289eee05d8d1
Luna Nova <git@lunnova.dev>
2023-04-01 14:50:33 -0700

    lun/discord: use armcord on aarch64

--- users/lun/gui/discord.nix
+++ users/lun/gui/discord.nix
@@ -1,12 +1,14 @@
 { lib, pkgs, flakeArgs, ... }:
 {
-  home.packages = lib.optionals (pkgs.system == "x86_64-linux") [
-    (flakeArgs.replugged-nix-flake.lib.makeDiscordPlugged {
-      inherit pkgs;
-      withOpenAsar = true;
-      themes = pkgs.powercord-themes;
-      plugins = pkgs.powercord-plugins;
-      extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
-    })
-  ];
+  home.packages =
+    if (pkgs.system == "x86_64-linux") then
+      [
+        (flakeArgs.replugged-nix-flake.lib.makeDiscordPlugged {
+          inherit pkgs;
+          withOpenAsar = true;
+          themes = pkgs.powercord-themes;
+          plugins = pkgs.powercord-plugins;
+          extraElectronArgs = "--ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling";
+        })
+      ] else [ pkgs.armcord ];
 }
