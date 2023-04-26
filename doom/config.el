49e8a78b0586de084ca43fa4d13e2baa154e8f47
Luna Nova <git@lunnova.dev>
2023-04-25 21:39:59 -0700

    doom: use outrun-electric theme

--- doom/config.el
+++ doom/config.el
@@ -43,7 +43,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night)
+(setq doom-theme 'doom-outrun-electric)
 
 (setq doom-font (font-spec :family "Fira Code" :size 10))
 
