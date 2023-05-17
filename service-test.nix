e36692240f8be738283ffdb80b1f08d860680a9a
Luna Nova <git@lunnova.dev>
2023-05-16 18:45:44 -0700

    test unprivileged service deployment

---
+++ service-test.nix
@@ -0,0 +1,49 @@
+_:
+{
+  # nixos module for host system which enables systemd lingering
+  # lingering.users = [ "user1" "user2" ];
+  # This allows systemd user services for these users to start on system boot.
+  hostNixosModule = { pkgs, lib, config, ... }:
+    let
+      lingerTouch = lib.concatMapStringsSep "\n"
+        (user: ''
+          touch /var/lib/systemd/linger/${user}
+        '')
+        config.lingering.users;
+    in
+    {
+      options = {
+        lingering = {
+          users = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = false;
+            description = ''
+              Enable lingering for the subset of users that are declared in
+              `users.users`.
+            '';
+          };
+        };
+      };
+      config = {
+        system.activationScripts = {
+          enableLingering = ''
+            # remove all existing lingering users
+            rm -r /var/lib/systemd/linger
+            mkdir /var/lib/systemd/linger
+            # enable for the subset of declared users
+            ${lingerTouch}
+          '';
+        };
+      };
+    };
+  singleServiceDeployProfile = { modules, services, ... }:
+    {
+      profile = pkgs.buildEnv
+        {
+          name = "deployProfile";
+          paths = [
+
+          ];
+        };
+    };
+}
