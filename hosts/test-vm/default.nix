d33ecf45b124e0db793d098501b75198c7da1f82
Luna Nova <git@lunnova.dev>
2023-05-16 18:45:44 -0700

    test unprivileged service deployment
    wip

---
+++ hosts/test-vm/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, lib, ... }:
+let
+  name = "test";
+in
+{
+  config = {
+    sconfig.machineId = "2e96a6dde38fec8bed7dd1c0d88fa7c5";
+    system.stateVersion = "23.05";
+
+    hardware.cpu.amd.updateMicrocode = true;
+
+    lun.persistence.enable = true;
+    fileSystems = {
+      "/" = {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [ "defaults" "size=2G" "mode=755" ];
+      };
+      "/boot" = {
+        device = "/dev/disk/by-partlabel/${name}_esp";
+        fsType = "vfat";
+        neededForBoot = true;
+        options = [ "discard" "noatime" ];
+      };
+      "/persist" = {
+        device = "/dev/disk/by-partlabel/${name}_persist";
+        fsType = "btrfs";
+        neededForBoot = true;
+        options = [ "subvol=@persist" "nodev" "nosuid" ];
+      };
+      "/nix" = {
+        neededForBoot = true;
+      };
+    };
+    swapDevices = lib.mkForce [ ];
+    boot.resumeDevice = "";
+  };
+}
