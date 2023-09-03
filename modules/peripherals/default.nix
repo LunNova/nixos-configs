f8fdd4b45c8669c8fc988d236e2694e211e3196d
Luna Nova <git@lunnova.dev>
2023-09-03 11:49:39 -0700

    peripherals: disable peripherals which require graphical apps if not profiles.graphical

--- modules/peripherals/default.nix
+++ modules/peripherals/default.nix
@@ -1,39 +1,40 @@
 { config, pkgs, lib, ... }:
 {
-  config = {
-    services.xserver.libinput = {
-      # Enable touchpad/mouse
-      enable = true;
-      # Disable mouse accel
-      mouse = { accelProfile = "flat"; };
-    };
+  config = lib.mkMerge [
+    {
+      services.fwupd.enable = true;
+      hardware.wirelessRegulatoryDatabase = true;
+      hardware.enableRedistributableFirmware = true;
+      # use with piper for gaming mouse configuration
+      # services.ratbagd.enable = true;
 
-    services.fwupd.enable = true;
-    hardware.keyboard.zsa.enable = true;
-    hardware.steam-hardware.enable = pkgs.system == "x86_64-linux";
-    hardware.enableRedistributableFirmware = true;
-    hardware.wirelessRegulatoryDatabase = true;
-    hardware.ledger.enable = true;
-    # use with piper for gaming mouse configuration
-    # services.ratbagd.enable = true;
+      # no USB wakeups
+      # see: https://github.com/NixOS/nixpkgs/issues/109048
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="disabled"
+      '';
+    }
 
-    # no USB wakeups
-    # see: https://github.com/NixOS/nixpkgs/issues/109048
-    services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="disabled"
-    '';
+    (lib.mkIf config.lun.profiles.graphical {
+      services.xserver.libinput = {
+        # Enable touchpad/mouse
+        enable = true;
+        # Disable mouse accel
+        mouse = { accelProfile = "flat"; };
+      };
 
-    services.udev.packages = lib.optionals (pkgs.system == "x86_64-linux") [
-      pkgs.lun.vial
-      pkgs.via
-    ];
-    environment.systemPackages = lib.optionals (pkgs.system == "x86_64-linux") [
-      pkgs.lun.vial
-      pkgs.via
-    ];
+      # udev rule for zsa oryx
+      hardware.keyboard.zsa.enable = true;
+      # steam controller and index headset, only works on x86_64 as of 202309
+      hardware.steam-hardware.enable = pkgs.system == "x86_64-linux";
+      hardware.ledger.enable = true;
 
-    # FIXME: xone doesn't work with wireless, seems unmaintained?
-    # find something better or patch it
-    # hardware.xone.enable = true;
-  };
+      services.udev.packages = [ pkgs.lun.vial ];
+      environment.systemPackages = [ pkgs.lun.vial ];
+
+      # FIXME: xone doesn't work with wireless, seems unmaintained?
+      # find something better or patch it
+      # hardware.xone.enable = true;
+    })
+  ];
 }
