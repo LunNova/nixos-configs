32a89f0b3e8abf63e09eef2a67bc45f66658e987
Luna Nova <git@lunnova.dev>
2023-09-04 11:21:51 -0700

    workarounds: disable systemd-udev-trigger workaround

--- modules/workarounds.nix
+++ modules/workarounds.nix
@@ -3,10 +3,11 @@
   config = {
     # networkd just stops managing my wlan0 after some generation changes #195777
     # https://github.com/NixOS/nixpkgs/issues/195777#issuecomment-1633546490
-    system.activationScripts = {
-      restart-udev = ''
-        ${pkgs.systemd}/bin/systemctl restart systemd-udev-trigger.service
-      '';
-    };
+    # FIXME: kosame resets display-manager when this is used
+    # system.activationScripts = {
+    #   restart-udev = ''
+    #     ${pkgs.systemd}/bin/systemctl restart systemd-udev-trigger.service
+    #   '';
+    # };
   };
 }
