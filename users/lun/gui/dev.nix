4ebb11a0487a3fd7b2d58ceb72548914bfd91af9
Luna Nova <git@lunnova.dev>
2023-10-05 19:04:33 -0700

    lun: swap clion for rust-rover

--- users/lun/gui/dev.nix
+++ users/lun/gui/dev.nix
@@ -8,8 +8,8 @@
       nixd
       flakeArgs.deploy-rs.packages.${pkgs.system}.default
     ] ++ lib.optionals (pkgs.system == "x86_64-linux") [
-      jetbrains.clion
       jetbrains.idea-ultimate
+      jetbrains.rust-rover
     ];
 
     programs.vscode = {
