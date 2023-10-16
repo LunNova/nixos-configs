0bc3c34da01ca3da38fcc68d8aea89187bebe38c
Luna Nova <git@lunnova.dev>
2023-10-16 10:16:53 -0700

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/b2a33ede1b5ba9c3d8b79f8d42dec4adde89649e' (2023-10-16)
      → 'github:nix-community/emacs-overlay/67acc54cceef38a92ba132e121659fef97698184' (2023-10-16)
    • Updated input 'nix-gaming':
        'github:fufexan/nix-gaming/42430485ab19049d69c81372ead35d6e683a94ac' (2023-10-16)
      → 'github:fufexan/nix-gaming/8fb703eaf958963957390285e992a68ce5c056ff' (2023-10-16)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/5e4c2ada4fcd54b99d56d7bd62f384511a7e2593' (2023-10-11)
      → 'github:NixOS/nixpkgs/982b24c40e743793c966b47b3bb3699881489ae0' (2023-10-15)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "lun's system config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/release-22.05";
     nixpkgs-cura-testing.url = "github:LunNova/nixpkgs/bd7de0e7c17a16885fbe25ffb7c266fffb65dfb9";
     home-manager.url = "github:nix-community/home-manager/master";
