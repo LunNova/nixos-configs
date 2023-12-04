62e1a2cf42e7f55f08203deb287b2c3059dfad18
Luna Nova <git@lunnova.dev>
2023-12-03 17:04:26 -0800

    lun/dev: add rehex hex editor

--- users/lun/gui/dev.nix
+++ users/lun/gui/dev.nix
@@ -6,6 +6,7 @@
       glxinfo
       vulkan-tools
       nixd
+      rehex
       flakeArgs.deploy-rs.packages.${pkgs.system}.default
     ] ++ lib.optionals (pkgs.system == "x86_64-linux") [
       jetbrains.idea-ultimate
