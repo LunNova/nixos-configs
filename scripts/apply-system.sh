249245fe9861f0de50f3df970e6739d87860a463
Luna Nova <git@lunnova.dev>
2024-01-04 16:05:01 -0800

    scripts/apply-system: simplify and use nix build --profile

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,41 +1,46 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
+
+if [[ ! -f /etc/NIXOS ]]; then
+	echo "This script is only for nixos systems"
+	exit 1
+fi
+if [[ $(id -u) -ne 0 ]]; then
+	echo "Please run as root"
+	exit 1
+fi
+
+profile="/nix/var/nix/profiles/system"
+flakeref=".#nixosConfigurations.$(hostname).config.system.build.toplevel"
 
 # This is a bodge.
 # amayadori's boot SSD sometimes seems to drop recent writes when it hangs and reboots
 function sync_pls() {
 	sync
-	sudo btrfs filesystem sync /nix/store/ || true
-	sudo btrfs filesystem sync /nix/var/nix/ || true
-	sync
+	if command -v btrfs >/dev/null; then
+		btrfs filesystem sync /nix/store/ || true
+		btrfs filesystem sync "$profile" || true
+	fi
 }
 
 cd "$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
 
-echo "Activating ${1:-toplevel}"
-$(which nom 2>/dev/null || which nix) build -j 4 -o tmp-system-build --keep-going ".#nixosConfigurations.$(hostname).config.system.build.toplevel"
-
-profile="/nix/var/nix/profiles/system"
-pathToConfig="$(readlink -f tmp-system-build)"
-rm tmp-system-build
+echo "Activating $flakeref (spec ${1:-toplevel}) as $profile"
+$(which nom 2>/dev/null || which nix) build  -j 4 --no-link --profile "$profile" --keep-going "$flakeref"
 
 sync_pls
 sleep 1
 
-sudo nix-env -p "$profile" --set "$pathToConfig"
-
 if [[ -v 1 ]]; then
-	sudo "$pathToConfig/specialisation/$1/bin/switch-to-configuration" switch
+	"$profile/specialisation/$1/bin/switch-to-configuration" switch
 else
-	sudo "$pathToConfig/bin/switch-to-configuration" switch
+	"$profile/bin/switch-to-configuration" switch
 fi
 
 sync_pls
 
-sudo "$pathToConfig/bin/switch-to-configuration" boot
+"$profile/bin/switch-to-configuration" boot
 
 # home-manager switch used to handle this?
 # since swapping to home-manager.nixosModules.home-manager seem to need to do this to get changes to happen immediately
-sudo systemctl restart home-manager-\* --all
-
-sync_pls
+systemctl restart home-manager-\* --all
