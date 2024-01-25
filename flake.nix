339f087badf5b7198d483b92f97ba041f3f60011
Luna Nova <git@lunnova.dev>
2024-01-25 08:01:14 -0800

    flake: use nixos-unstable-small because nixos-unstable is stuck

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "lun's system config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.11";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
