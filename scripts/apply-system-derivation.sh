b974edeaa37914931bfa5458e4fa50f12e469bef
Luna Nova <git@lunnova.dev>
2024-01-25 09:50:39 -0800

    scripts/apply-system: split out sudo part

---
+++ scripts/apply-system-derivation.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $(id -u) -ne 0 ]]; then
+	echo "Please run as root"
+	exit 1
+fi
+
+profile="$1"; shift
+result="$2"; shift
+
+# This is a bodge.
+# amayadori's boot SSD sometimes seems to drop recent writes when it hangs and reboots
+function sync_pls() {
+	sync
+	if command -v btrfs >/dev/null; then
+		btrfs filesystem sync /nix/store/ || true
+		btrfs filesystem sync "$profile" || true
+	fi
+}
+
+sync_pls
+sleep 1
+
+nix-env --profile "$profile" --set "$result"
+
+sync_pls
+
+if [[ -v 1 ]]; then
+	"$profile/specialisation/$1/bin/switch-to-configuration" switch
+else
+	"$profile/bin/switch-to-configuration" switch
+fi
+
+sync_pls
+
+"$profile/bin/switch-to-configuration" boot
+
+# home-manager switch used to handle this?
+# since swapping to home-manager.nixosModules.home-manager seem to need to do this to get changes to happen immediately
+systemctl restart home-manager-\* --all
--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -5,42 +5,19 @@ if [[ ! -f /etc/NIXOS ]]; then
 	echo "This script is only for nixos systems"
 	exit 1
 fi
-if [[ $(id -u) -ne 0 ]]; then
-	echo "Please run as root"
-	exit 1
-fi
 
 profile="/nix/var/nix/profiles/system"
 flakeref=".#nixosConfigurations.$(hostname).config.system.build.toplevel"
 
-# This is a bodge.
-# amayadori's boot SSD sometimes seems to drop recent writes when it hangs and reboots
-function sync_pls() {
-	sync
-	if command -v btrfs >/dev/null; then
-		btrfs filesystem sync /nix/store/ || true
-		btrfs filesystem sync "$profile" || true
-	fi
-}
-
 cd "$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
 
 echo "Activating $flakeref (spec ${1:-toplevel}) as $profile"
-$(which nom 2>/dev/null || which nix) build  -j 4 --no-link --profile "$profile" --keep-going "$flakeref"
 
-sync_pls
-sleep 1
+nix="$(which nom 2>/dev/null || which nix)"
+result="$("$nix" build -j 4 --no-link --print-out-paths --keep-going "$flakeref")"
 
-if [[ -v 1 ]]; then
-	"$profile/specialisation/$1/bin/switch-to-configuration" switch
+if [ -f "$result/bin/switch-to-configuration" ]; then
+	sudo scripts/apply-system-derivation.sh "$profile" "$result"
 else
-	"$profile/bin/switch-to-configuration" switch
+	exit 1
 fi
-
-sync_pls
-
-"$profile/bin/switch-to-configuration" boot
-
-# home-manager switch used to handle this?
-# since swapping to home-manager.nixosModules.home-manager seem to need to do this to get changes to happen immediately
-systemctl restart home-manager-\* --all
