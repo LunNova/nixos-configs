970ab2eb1c2f68f956e1b6bce9a21bda9f86560f
Luna Nova <git@lunnova.dev>
2025-03-20 15:34:25 -0700

    WIP aoame

--- hosts/aoame/default.nix
+++ hosts/aoame/default.nix
@@ -6,7 +6,7 @@ in
   imports = [
     flakeArgs.x1e-nixos-config.nixosModules.x1e
     flakeArgs.disko.nixosModules.disko
-    flakeArgs.lanzaboote.nixosModules.lanzaboote
+    # flakeArgs.lanzaboote.nixosModules.lanzaboote
     ./disk-config.nix
   ];
   config = {
@@ -31,6 +31,18 @@ in
       emergencyAccess = true;
     };
 
+    zramSwap.enable = true;
+    zramSwap.memoryPercent = 30;
+    # not much RAM available to build with so limit jobs and cores
+    nix.settings.max-jobs = 3;
+    nix.settings.cores = 6;
+    #  services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+    services.displayManager.defaultSession = "none+i3";
+    lun.virtualisation.enable = lib.mkForce false;
+
+    services.udisks2.enable = true; # required for automounting with udiskie
+    boot.plymouth.enable = lib.mkForce false;
+    users.mutableUsers = false;
     hardware.enableRedistributableFirmware = true;
 
     networking.networkmanager = {
@@ -39,5 +51,21 @@ in
     };
 
     hardware.bluetooth.enable = true;
+    lun.persistence.enable = true;
+    lun.persistence.dirs = [
+      "/tmp"
+      "/var/lib/sddm"
+    ];
+    fileSystems = {
+      "/" = {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [
+          "defaults"
+          "size=2G"
+          "mode=755"
+        ];
+      };
+    };
   };
 }
--- hosts/aoame/disk-config.nix
+++ hosts/aoame/disk-config.nix
@@ -7,19 +7,34 @@
         content = {
           type = "gpt";
           partitions = {
+
+            # ESP partition, or "boot" partition as you may call it. In theory,
+            # this config will support VPSes with both EFI and BIOS boot modes.
             ESP = {
-              size = "512M";
+              alignment = 2048;
+              label = "_esp";
+              name = "ESP";
+              # Reserve 512MB of space per my own need. If you use more/less
+              # on your boot partition, adjust accordingly.
+              size = "1024M";
               type = "EF00";
+              # Use the second highest priority so it's before the remaining space
+              priority = 1;
+              # Format as FAT32
               content = {
                 type = "filesystem";
                 format = "vfat";
+                # Use as boot partition. Disko use the information here to mount
+                # partitions on disk image generation. Use the same settings as
+                # fileSystems.*
                 mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
+                mountOptions = [ "fmask=0077" "dmask=0077" ];
               };
             };
-            luks = {
+
+
+            persist = {
+              alignment = 2048;
               size = "100%";
               content = {
                 type = "luks";
@@ -34,24 +49,35 @@
                 content = {
                   type = "btrfs";
                   extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" ];
+                  subvolumes =
+                    let
+                      commonOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        "space_cache=v2"
+                      ];
+                    in
+                    {
+
+                      # nix store
+                      "/@nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = commonOptions;
+                      };
+
+                      # Persistent data
+                      "/@persist" = {
+                        mountpoint = "/persist";
+                        mountOptions = commonOptions ++ [
+                          "nodev"
+                          "nosuid"
+                        ];
+                      };
+                      "/swap" = {
+                        mountpoint = "/.swapvol";
+                        swap.swapfile.size = "16G";
+                      };
                     };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "16G";
-                    };
-                  };
                 };
               };
             };
