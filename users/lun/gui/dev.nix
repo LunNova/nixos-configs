23e5520db125dace5ec01630eb72703d6d6f47c4
Luna Nova <git@lunnova.dev>
2025-03-29 11:07:52 -0700

    lun/dev: improve VS Code font selection

--- users/lun/gui/dev.nix
+++ users/lun/gui/dev.nix
@@ -64,7 +64,7 @@ in
       package = pkgs.vscode.fhs;
       profiles.default.userSettings = {
         "workbench.colorTheme" = "Tomorrow Night Blue";
-        "editor.fontFamily" = ''"SF Mono Regular", "SF Mono", SF Mono, SFMono-Regular, monospace, 'Font Awesome 6 Brands', 'Font Awesome 6 Free', mono'';
+        "editor.fontFamily" = ''"SF Mono Regular", "SF Mono", SF Mono, SFMono-Regular, monospace, 'Font Awesome 6 Brands', 'Font Awesome 6 Free', Symbola, 'Last Resort High\-Efficiency', 'Last Resort High-Efficiency', mono'';
         # Add other settings as needed
       };
       profiles.default.extensions = with pkgs.vscode-extensions; [
