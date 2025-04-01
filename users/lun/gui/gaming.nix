a91325bd842d0fc3dd5955e9069b5c812b763b66
Luna Nova <git@lunnova.dev>
2025-03-31 19:03:56 -0700

    lun/gaming: set up openxr monado

--- users/lun/gui/gaming.nix
+++ users/lun/gui/gaming.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, lun-profiles, ... }:
+{ pkgs, config, lib, lun-profiles, ... }:
 {
   home.packages = with pkgs; [
     # osu-lazer not currently playing
@@ -8,4 +8,16 @@
     pkgs.lun.wine
     # TODO: try bottles instead of lutris
   ];
+
+  xdg.configFile."openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+  xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+    {
+      "config": ["${config.xdg.dataHome}/Steam/config"],
+      "external_drivers": null,
+      "jsonid": "vrpathreg",
+      "log": ["${config.xdg.dataHome}/Steam/logs"],
+      "runtime" : ["${pkgs.opencomposite}/lib/opencomposite"],
+      "version" : 1
+    }
+  '';
 }
