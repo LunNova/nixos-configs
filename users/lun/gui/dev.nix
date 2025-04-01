0acf30f36b0bc12510e6410e5292d7d3888f0021
Luna Nova <git@lunnova.dev>
2025-03-31 19:03:36 -0700

    lun/gui: enable activitywatch

--- users/lun/gui/dev.nix
+++ users/lun/gui/dev.nix
@@ -72,6 +72,30 @@ in
       ];
     };
 
+    services.activitywatch = {
+      enable = true;
+      package = pkgs.aw-server-rust;
+      watchers = {
+        aw-watcher-afk = {
+          package = pkgs.activitywatch;
+          settings = {
+            timeout = 30;
+            poll_time = 5;
+          };
+        };
+
+        aw-watcher-window = {
+          package = pkgs.activitywatch;
+          settings = {
+            aw-watcher-window = {
+              poll_time = 5.0;
+              exclude_title = false;
+            };
+          };
+        };
+      };
+    };
+
     services.ssh-agent.enable = true;
 
     systemd.user.services.ssh-agent-add-keys = {
