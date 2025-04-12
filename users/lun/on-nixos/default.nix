9245144e2020e7a8dfc0778679d7620b000ba7cb
Luna Nova <git@lunnova.dev>
2025-04-12 16:18:53 -0700

    lun/on-nixos: disable graphical modules if no graphical profile

--- users/lun/on-nixos/default.nix
+++ users/lun/on-nixos/default.nix
@@ -1,11 +1,12 @@
 { nixosConfig, ... }:
 {
   # Modules which depend on nixosConfig being set and are otherwise not imported
-  imports = if nixosConfig == null then [ ] else [
+  imports = if nixosConfig == null then [ ] else
+  (if nixosConfig.lun.profiles.graphical then [
     ./audio.nix
     ./input-remapper.nix
     ./blueman.nix
     ./barrier.nix
     ./kdeconfig.nix
-  ];
+  ] else [ ]);
 }
