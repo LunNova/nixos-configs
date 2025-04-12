7b43fe3d7ae254582009f4bb0cbda849118c934e
Luna Nova <git@lunnova.dev>
2025-04-12 16:21:38 -0700

    spawn: use cgroup exit type so vscode behaves correctly through spawn

--- packages/spawn/default.nix
+++ packages/spawn/default.nix
@@ -4,19 +4,21 @@
 , util-linux
 , lib
 }:
-# Spawn a process inside systemd user scope
+# Spawn a process inside systemd user slice
+# Uses ExitType=cgroup to handle apps like vscode that are shortlived and spawn another process
+# and apps that are long lived and don't spawn a new process without needing to guess
+# or specify an exec mode
 (writeShellApplication {
   name = "spawn";
 
   runtimeInputs = [ coreutils systemd util-linux ];
 
-  # TODO -p ReadOnlyPaths=/ -p ReadWritePaths / bubblewrap?
   text = ''
     [ "$#" -ge 1 ] || exit 1
     read -ra cmd <<<"$*"
     program="''${cmd[0]}"
     name="$(basename "$program")"
     uuid="$(uuidgen)"
-    exec systemd-run --user --scope --unit "run-$name-$uuid" "''${cmd[@]}"
+    exec systemd-run --property=ExitType=cgroup --slice-inherit --slice="sl-$name-$uuid" --user --unit "run-$name-$uuid" "''${cmd[@]}"
   '';
 }) // { meta.platforms = lib.platforms.linux; }
