f46f50cba35edc004e639b7a2e4e0965e264e497
Luna Nova <git@lunnova.dev>
2025-04-12 16:22:06 -0700

    wayland: set up wrapper for Hyprland

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -1,17 +1,35 @@
-{ pkgs, config, flakeArgs, ... }:
+{ pkgs, lib, config, flakeArgs, ... }:
 {
-  security.pam.services.hyprlock = { };
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    package = flakeArgs.hyprland.packages.${pkgs.system}.hyprland; # pkgs.hyprland;
-    # portalPackage = pkgs.xdg-desktop-portal-hyprland;
-    portalPackage = flakeArgs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
-    systemd.setPath.enable = true;
+  config = lib.mkIf config.lun.profiles.graphical {
+    security.pam.services.hyprlock = { };
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      package = flakeArgs.hyprland.packages.${pkgs.system}.hyprland;
+      portalPackage = flakeArgs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      systemd.setPath.enable = true;
+    };
+    environment.systemPackages = [
+      pkgs.at-spi2-atk
+      pkgs.hyprpanel
+      pkgs.hyprcursor
+    ];
+    environment.sessionVariables = {
+      GTK_MODULES = "gail:atk-bridge";
+      OOO_FORCE_DESKTOP = "gnome";
+      GNOME_ACCESSIBILITY = "1";
+      QT_ACCESSIBILITY = "1";
+      QT_LINUX_ACCESSIBILITY_ALWAYS_ON = "1";
+    };
+    programs.uwsm.waylandCompositors.hyprland.binPath = lib.mkForce "/run/wrappers/bin/Hyprland";
+    security.wrappers."Hyprland" = {
+      setuid = false;
+      permissions = "u+rx,g+rx,o+rx";
+      owner = "root";
+      group = "root";
+      # cap_sys_nice needed for asynchronous reprojection
+      capabilities = "cap_sys_nice+eip";
+      source = lib.getExe' config.programs.hyprland.package "hyprland";
+    };
   };
-  environment.systemPackages = [
-    pkgs.hyprpanel
-    pkgs.hyprcursor
-    config.programs.hyprland.package
-  ];
 }
