c56e911026eb3e03e7973e5d92ed5c3884ee9a7c
Luna Nova <git@lunnova.dev>
2025-09-13 14:12:15 -0700

    lun/kdeconfig: use kdePackages.qttools instead of libsForQt5

--- users/lun/on-nixos/kdeconfig.nix
+++ users/lun/on-nixos/kdeconfig.nix
@@ -52,7 +52,7 @@ in
       confdir="''${XDG_CONFIG_HOME:-$HOME/.config}"
       ${builtins.concatStringsSep "\n" lines}
 
-      $DRY_RUN_CMD ${pkgs.libsForQt5.qt5.qttools.bin}/bin/qdbus org.kde.KWin /KWin reconfigure || echo "KWin reconfigure failed"
+      $DRY_RUN_CMD ${pkgs.kdePackages.qttools}/bin/qdbus org.kde.KWin /KWin reconfigure || echo "KWin reconfigure failed"
       for i in {0..10}; do
         $DRY_RUN_CMD ${pkgs.dbus}/bin/dbus-send --type=signal /KGlobalSettings org.kde.KGlobalSettings.notifyChange int32:$i int32:0 || echo "KGlobalSettings.notifyChange failed"
       done
