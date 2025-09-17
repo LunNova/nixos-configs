fc4214161e5d04308325f67c4705b11be5ac6f1b
Luna Nova <git@lunnova.dev>
2025-09-16 19:59:19 -0700

    lix: introduce an attempt at a hack for load average. doesn't work

--- modules/lix.nix
+++ modules/lix.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }:
 {
   nix.package = pkgs.lix;
+
+  # HACK: sleep for 1s in pre-build-hook until load average is below some number
+  # cpus=$(${pkgs.coreutils}/bin/nproc)
+  # cpus=$((cpus / 2)) # assume hyperthreading
+  # nix.settings.pre-build-hook = pkgs.writeScript "nix-pre-build-hook" ''#!/usr/bin/env bash
+  #   set -euf
+
+  #   cpus=20
+  #   while true; do
+  #     load=$(< /proc/loadavg)
+  #     load_int=''${load%%.*}
+
+  #     [ "$load_int" -gt "$cpus" ] || break
+  #     sleep 1
+  #   done
+  # '';
 }
