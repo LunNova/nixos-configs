ae68811fc830ef06921dba2a951be992762b7085
Luna Nova <git@lunnova.dev>
2025-09-16 19:58:26 -0700

    flake-registry: mostly deactivate pending better ideas

--- modules/flake-registry.nix
+++ modules/flake-registry.nix
@@ -1,26 +1,23 @@
 # Approach originally from https://github.com/tejing1/
-{ pkgs, config, lib, flakeArgs, ... }:
-let
-  inherit (lib.strings) escapeNixIdentifier;
-  inherit (flakeArgs.self.lib) mkFlake;
-in
+{ pkgs, ... }:
 {
   config = {
-    system.configurationRevision = lib.mkIf (flakeArgs.self ? rev) flakeArgs.self.rev; # set configurationRevision if available
+    #system.configurationRevision = lib.mkIf (flakeArgs.self ? rev) flakeArgs.self.rev; # set configurationRevision if available
 
     # Legacy compat for non-flake uses:
     # Update immediately by using paths instead of needing to get new NIX_PATH env
     nix.nixPath = [ "/etc/nix/path" ];
-    environment.etc."nix/path/nixpkgs".source = flakeArgs.nixpkgs;
+    environment.etc."nix/path/nixpkgs".source = pkgs;
 
     #nixpkgs=input nixpkgs
-    nix.registry.nixpkgs.flake = flakeArgs.nixpkgs;
-    nix.registry.nixpkgs-stable.flake = flakeArgs.nixpkgs-stable;
+    nix.registry.nixpkgs.flake = pkgs;
+    #nix.registry.nixpkgs-stable.flake = flakeArgs.nixpkgs-stable;
 
     #nix.registry.nixos-config.flake = lun;
     #pkgs = pkgs provided to this system
-    nix.registry.pkgs.flake =
-      mkFlake pkgs { config = flakeArgs.self; }
-        "{config,...}: {legacyPackages.${escapeNixIdentifier pkgs.system}=config.pkgs.${escapeNixIdentifier pkgs.system};}";
+    # nix.registry.pkgs.flake = flakeArgs.nixpkgs;
+    # FIXME: Figure out better way to get a `pkgs` that is this flake's overlayed pkgs available as `pkgs` flake
+    # mkFlake pkgs { config = flakeArgs.self; }
+    #   "{config,...}: {legacyPackages.${escapeNixIdentifier pkgs.system}=config.pkgs.${escapeNixIdentifier pkgs.system};}";
   };
 }
