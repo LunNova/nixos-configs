0144989eb6f1644ad938397ad997e93553b7c26c
Luna Nova <git@lunnova.dev>
2025-10-26 12:51:00 -0700

    hosts/hoshitsuki: remove duplicate noatime

--- hosts/hoshitsuki/default.nix
+++ hosts/hoshitsuki/default.nix
@@ -2,7 +2,7 @@
 let
   name = "hoshitsuki";
   swap = null; #"/dev/disk/by-partlabel/${name}_swap";
-  btrfsOpts = [ "rw" "noatime" "compress=zstd" "space_cache=v2" "noatime" ];
+  btrfsOpts = [ "rw" "noatime" "compress=zstd" "space_cache=v2" ];
   btrfsSsdOpts = btrfsOpts ++ [ "ssd" "nodiscard" ];
 in
 {
