5b24ed2e6cf5317062786980bf5a50a6fe4097f4
Luna Nova <git@lunnova.dev>
2026-02-17 22:00:09 -0800

    overlay-nixpkgs: fix libratbag hidraw handling for asus TODO upstream me

--- overlay-nixpkgs.nix
+++ overlay-nixpkgs.nix
@@ -1,2 +1,15 @@
-_: _: _:
-{ }
+_: final: prev:
+{
+  libratbag = prev.libratbag.overrideAttrs (old: {
+    src = final.fetchFromGitHub {
+      owner = "libratbag";
+      repo = "libratbag";
+      rev = "874c01732a7d3c074baefd8055c0d3efe8c9a935";
+      hash = "sha256-O9DxwAieUEy+otwDSM2412vCCQJkHIrDOPVYevg0l44=";
+    };
+
+    patches = (old.patches or [ ]) ++ [
+      ./libratbag-hidraw-asus.patch
+    ];
+  });
+}
