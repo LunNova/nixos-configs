cf6cac9f60036b900aa8255b27a9ae2e1d7064df
Luna Nova <git@lunnova.dev>
2026-05-07 07:38:26 -0700

    modules/printers: add epson-escpr2 and drop sane-airscan for now

--- modules/printers.nix
+++ modules/printers.nix
@@ -3,9 +3,10 @@
   options.lun.print.enable = lib.mkEnableOption "Enable printing and scanning";
   config = lib.mkIf config.lun.print.enable {
     services.printing.enable = true;
-    hardware.sane = {
-      enable = true;
-      extraBackends = [ pkgs.sane-airscan ];
-    };
+    services.printing.drivers = [
+      pkgs.epson-escpr2
+    ];
+    programs.system-config-printer.enable = true;
+    # FIXME: hardware.sane + sane-airscan should work for scanning, but it don't
   };
 }
