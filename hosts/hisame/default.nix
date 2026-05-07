687784b34a8b3cb095ec492f9c2d5a2a99cd42ff
Luna Nova <git@lunnova.dev>
2026-05-07 08:24:48 -0700

    hosts/hisame: horrifying large pile of cfg changes that should have been split

--- hosts/hisame/default.nix
+++ hosts/hisame/default.nix
@@ -1,11 +1,10 @@
 { config, pkgs, lib, ... }:
 let
   name = "hisame";
-  swap = "/dev/disk/by-partlabel/hisame_swap";
+  swap = "/dev/disk/by-partlabel/hisame_swap_2";
   btrfsOpts = [ "rw" "noatime" "compress=zstd" "space_cache=v2" "noatime" ];
   btrfsSsdOpts = btrfsOpts ++ [ "ssd" "discard=async" ];
   enableFbDevs = true;
-  gpuPatches = false;
   # env = {
   #   # kwin wayland tearing support requires this for now
   #   # https://invent.kde.org/plasma/kwin/-/merge_requests/927
@@ -22,8 +21,18 @@ let
   #   };
   # };
   env = {
-    XDP_COSMIC = lib.getExe pkgs.xdg-desktop-portal-cosmic;
+    KWIN_DRM_DISABLE_TRIPLE_BUFFERING = "1";
+    # KWIN_DRM_DEVICES = "/dev/dri/card1";
   };
+  rtl_bt_fw_file = builtins.fetchurl {
+    url =
+      "https://github.com/Arroquw/rtl8761bu_fw/raw/refs/heads/main/rtl8761bu_fw.bin";
+    sha256 = "sha256:1hdk46hly2ir4ccqcwlb48mvvpkwd3zc8vhhfhkbl770fszmn49d";
+  };
+  rtl8761_fw = pkgs.runCommandNoCC "rtl_bt-firmware" { } ''
+    mkdir -p $out/lib/firmware/rtl_bt
+    cp "${rtl_bt_fw_file}" "$out"/lib/firmware/rtl_bt/rtl8761bu_fw.bin
+  '';
 in
 {
   config = {
@@ -81,68 +90,99 @@ in
           FB_RADEON = lib.mkForce no;
         };
       }
-    ]) ++ lib.optionals gpuPatches [
+    ]
+    ) ++ [
       {
-        name = "THP";
+        name = "lun-cfg";
         patch = null;
-        structuredExtraConfig = with lib.kernel; {
-          READ_ONLY_THP_FOR_FS = lib.mkForce yes;
-        };
+        extraConfig = ''
+          PCI_P2PDMA y
+          DMABUF_MOVE_NOTIFY y
+          HSA_AMD y
+          HSA_AMD_SVM y
+          HSA_AMD_P2P y
+          PCI_SW_SWITCHTEC y
+          FONT_TER16x32 n
+        '';
       }
-      {
-        name = "amdgpu-no-ecc";
-        patch = ./kernel/amdgpu-no-ecc.patch;
-      }
-      {
-        name = "amdgpu_bo_fence_warn.patch";
-        patch = ./kernel/amdgpu_bo_fence_warn.patch;
-      }
-      {
-        name = "log-psp-resume";
-        patch = ./kernel/log-psp-resume.patch;
-      }
-      {
-        name = "amdgpu-pm-no-resume";
-        patch = ./kernel/amdgpu-pm-no-resume.patch;
-      }
-      # {
-      #   name = "arc-multigpu-buffer-length";
-      #   patch = ./kernel/arc-multigpu-buffer-length.patch;
-      # }
-      # {
-      #   name = "amdgpu-force-d3.patch";
-      #   patch = ./kernel/amdgpu-force-d3.patch;
-      # }
-      # {
-      #   name = "disable-acs-redir.patch";
-      #   patch = ./kernel/disable-acs-redir.patch;
-      # }
     ];
-    services.hardware.bolt.enable = true;
-    # specialisation.gnome.configuration = {
-    #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    #   services.xserver.desktopManager.gnome.enable = lib.mkForce true;
-    #   xdg.portal.extraPortals = lib.mkForce [
-    #     pkgs.xdg-desktop-portal-gnome
-    #   ];
-    # };
-    # specialisation.gnome-nvk.configuration = {
-    #   lun.nvk.enable = true;
-    #   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-    #   services.xserver.desktopManager.gnome.enable = lib.mkForce true;
-    #   xdg.portal.extraPortals = lib.mkForce [
-    #     pkgs.xdg-desktop-portal-gnome
-    #   ];
-    # };
-    services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+    boot.zswap.enable = true;
+
+    nix.buildMachines = [
+      {
+        protocol = "ssh";
+        system = "x86_64-linux";
+        sshUser = "lun";
+        hostName = "hoshitsuki-nixos.home.moonstruck.dev";
+        supportedFeatures = [ "kvm" ]; # 64GB RAM
+        maxJobs = 5;
+        # speedFactor = 1;
+      }
+      {
+        protocol = "ssh";
+        system = "aarch64-darwin";
+        sshUser = "lun";
+        hostName = "luns-mbp.home.moonstruck.dev";
+        supportedFeatures = [ "kvm" "big-parallel" ];
+        maxJobs = 3;
+        speedFactor = 2;
+      }
+      {
+        protocol = "ssh";
+        system = "aarch64-darwin";
+        sshUser = "lun";
+        hostName = "lun-mbp.home.moonstruck.dev";
+        supportedFeatures = [ "kvm" "big-parallel" ];
+        maxJobs = 2;
+        speedFactor = 1;
+      }
+      {
+        protocol = "ssh";
+        system = "aarch64-linux";
+        sshUser = "lun";
+        hostName = "lun-aoame.home.moonstruck.dev";
+        supportedFeatures = [ "big-parallel" "kvm" ]; # 64GB RAM
+        maxJobs = 2;
+        # speedFactor = 1;
+      }
+      {
+        protocol = "ssh";
+        system = "x86_64-linux";
+        sshUser = "lun";
+        hostName = "tsukikage-nixos.home.moonstruck.dev"; # 256GB RAM
+        supportedFeatures = [ "big-parallel" "kvm" ];
+        maxJobs = 3;
+        # speedFactor = 1;
+      }
+      {
+        protocol = "ssh";
+        system = "x86_64-linux";
+        sshUser = "lun";
+        hostName = "tsukiakari-nixos.home.moonstruck.dev";
+        supportedFeatures = [ "big-parallel" "kvm" ]; # 512GB RAM
+        maxJobs = 4;
+        # speedFactor = 1;
+      }
+    ];
+    nix.settings.builders = lib.mkForce "@/etc/nix/machines";
+    hardware.firmware = [ rtl8761_fw ];
+    hardware.xone.enable = true;
+    environment.systemPackages = [
+      pkgs.lun.obsbot-camera-control
+    ];
+
+    # services.hardware.bolt.enable = true;
+    services.xserver.desktopManager.plasma6.enableQt5Integration = lib.mkForce false; # FIXME: pulls in kio-extras-kf5!!
     services.desktopManager.plasma6.enable = true;
     programs.kdeconnect.enable = true;
     networking.firewall = {
       allowedTCPPortRanges = [
         { from = 1714; to = 1764; } # KDE Connect
+        { from = 20000; to = 65535; } # high ports
       ];
       allowedUDPPortRanges = [
         { from = 1714; to = 1764; } # KDE Connect
+        { from = 20000; to = 65535; } # high ports
       ];
     };
     lun.amd-pstate.enable = true;
@@ -157,38 +197,36 @@ in
       wineGaming = true;
     };
 
-    services.resolved = {
-      enable = true;
-      llmnr = "true";
-      dnssec = "false";
-      fallbackDns = [
-        "1.1.1.1"
-        "8.8.8.8"
-      ];
-    };
-    services.nscd.enableNsncd = true;
-
     services.udev.extraRules = ''
       # make mount work for ntfs devices without specifying -t ntfs3
       SUBSYSTEM=="block", ENV{ID_FS_TYPE}=="ntfs", ENV{ID_FS_TYPE}="ntfs3"
-
-      # remove nvidia audio
-      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
+      # # remove nvidia audio
+      # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
     '';
 
     # boot.kernelModules = [ "nct6775" "zenpower" ];
     # Use zenpower rather than k10temp for CPU temperatures.
     # boot.extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
+    environment.pathsToLink = [
+      "/share/wireplumber"
+    ];
     boot.blacklistedKernelModules = [
-      # "nouveau"
+      "nouveau"
       "radeon"
+      # "snd_hda_intel"
+      # "amdgpu"
       # "sp5100_tco" # watchdog hardware doesn't work
       # "k10temp" # replaced by zenpower
     ];
-    services.power-profiles-daemon.enable = true;
+    services.upower.enable = true;
+    services.tuned.enable = true;
 
-    # use mainline kernel because we're on very recent hardware
-    boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    boot.extraModprobeConfig = ''
+      options nvidia_drm modeset=1 fbdev=1
+      options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_RegistryDwords=RMIntrLockingMode=1 NVreg_UsePageAttributeTable=1 NVreg_DynamicPowerManagement=0x02 NVreg_EnableGpuFirmware=0
+    '';
+
+    boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
     # Example with overridden source for testing 6.1
     # boot.kernelPackages =
@@ -208,38 +246,20 @@ in
     #   in
     #   pkgs.linuxPackagesFor kernel;
 
-
     lun.power-saving.enable = true;
     lun.efi-tools.enable = true;
 
+    services.gvfs.enable = false;
     services.xserver.videoDrivers = [ "nvidia" ];
     services.xserver.dpi = 96; # force 100% DPI
-    # services.xserver.drivers = lib.mkIf (config.lun.gpu-select.card != null) (lib.mkForce [{
-    #   name = "modesetting";
-    #   display = true;
-    #   deviceSection = ''
-    #     Option "kmsdev" "/dev/dri/${config.lun.gpu-select.card}"
-    #   '';
-    # }]);
-    lun.gpu-select.enable = true;
-    # lun.nvidia-gpu-standalone.enable = true; # enable nvidia gpu kernel modules and opengl/vulkan support only, no x stuff changes
-    lun.nvidia-gpu-standalone.delayXWorkaround = true; # enable nvidia gpu kernel modules and opengl/vulkan support only, no x stuff changes
-    # Need to use .production driver to match version in some docker containers right now
-    # Typically use .beta
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
+    lun.nvidia-gpu-standalone.delayXWorkaround = true;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production; # {production,beta} etc
+    hardware.nvidia.open = false;
     hardware.nvidia.modesetting.enable = true;
     hardware.nvidia.powerManagement.enable = true;
-    lun.openxr.enable = true;
+    # lun.openxr.enable = true; FIXME ENABLE
     boot.kernelModules = [ "nvidia_uvm" ];
-    # hardware.nvidia.powerManagement.finegrained = true;
-    lun.ml = {
-      enable = false; # FIXME: rocm https://github.com/NixOS/nixpkgs/issues/203949
-      gpus = [ "amd" ];
-    };
-    hardware.graphics = {
-      package = pkgs.lun.mesa.drivers;
-      package32 = pkgs.lun.mesa-i686.drivers;
-    };
+    systemd.services.networking.wants = [ "systemd-udev-settle.service" ];
     modules.media.audio.interfaces.scarlett2.enable = true;
 
     hardware.bluetooth.settings = {
@@ -251,32 +271,63 @@ in
     };
     services.pipewire = {
       enable = true;
+
       extraConfig.pipewire."92-latency" = {
         context.properties = {
           default.clock.quantum = 1024;
           default.clock.min-quantum = 1024;
           default.clock.max-quantum = 1024;
         };
-        # jack.properties = {
-        #   node.quantum = "256/48000";
-        # };
       };
-      wireplumber.extraConfig."10-bluez" = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.hfphsp-backend" = "native";
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-            "a2dp_sink"
-            "a2dp_source"
-            "bap_sink"
-            "bap_source"
-          ];
+
+      # udev
+      # Avid MBox 3 has two Analog outputs/inputs and two SPDIF Digital outputs/inputs
+      # > ATTRS{idVendor}=="0dba", ATTRS{idProduct}=="5000", ENV{ACP_PROFILE_SET}="avid-mbox3.conf"
+      wireplumber = {
+        enable = true;
+        configPackages = [
+          (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-stop-microphone-auto-adjust.lua" ''
+            table.insert (default_access.rules,{
+                matches = {
+                    {
+                        { "application.process.binary", "=", "electron" },
+                        { "application.process.binary", "=", "webcord" },
+                        { "application.process.binary", "=", "firefox" },
+                        { "application.process.binary", "=", "vesktop" },
+                        { "application.process.binary", "=", "discord" }
+                        { "application.process.binary", "=", ".Discord-wrapped" }
+                    }
+                },
+                default_permissions = "rx",
+            })
+          '')
+        ];
+        extraConfig = {
+          node.features.audio.control-port = true;
+          "10-alsaUseUCM" = {
+            "monitor.alsa.properties" = {
+              "alsa.use-acp" = true;
+              # "alsa.use-ucm" = true;
+            };
+          };
+          "10-bluez" = {
+            "monitor.bluez.properties" = {
+              "bluez5.enable-sbc-xq" = true;
+              "bluez5.enable-msbc" = true;
+              "bluez5.enable-hw-volume" = true;
+              "bluez5.hfphsp-backend" = "native";
+              "bluez5.roles" = [
+                "hsp_hs"
+                "hsp_ag"
+                "hfp_hf"
+                "hfp_ag"
+                "a2dp_sink"
+                "a2dp_source"
+                "bap_sink"
+                "bap_source"
+              ];
+            };
+          };
         };
       };
     };
@@ -330,11 +381,31 @@ in
         22000 # syncthing (hisame stays on LAN)
       ];
     };
-    # don't have enough ram for 32j/32c to make sense for all builds
-    nix.settings.max-jobs = 5;
-    nix.settings.cores = 16;
-    nix.settings.max-silent-time = 3600;
+    nix.settings.max-jobs = 4;
+    nix.settings.cores = 20;
+    # FIXME: ┃ error: Unexpected exception on the Lix daemon; this is a bug in Lix.
+    # ┃        We would appreciate a report of the circumstances it happened in at https://git.lix.systems/lix-project/lix.
+    # ┃        nix::ForeignException: kj/async-unix.c++:466: failed: epoll_ctl(eventPort.epollFd, EPOLL_CTL_DEL, fd, nullptr): Bad file descriptor
+    # ┃        stack: 7fc676fb7990 7fc67695d697 7fc676951b88 7fc676fa7f21 7fc676fa7c4e 7fc67694e730 7fc67693ebdd
+    nix.settings.max-silent-time = 11111;
+    boot.binfmt.emulatedSystems = lib.remove pkgs.hostPlatform.system [
+      "aarch64-linux"
+      "loongarch64-linux"
+      "riscv64-linux"
+      "x86_64-linux"
+    ];
+    nix.settings.extra-platforms = [
+      "aarch64-linux"
+      "i686-linux"
+    ];
 
+    lun.k3s = {
+      enable = true;
+      role = "agent";
+      serverAddr = "https://hoshitsuki-nixos.home.moonstruck.dev:6443";
+    };
+
+    lun.outofdateinator.enable = true;
     lun.persistence.enable = true;
     lun.persistence.dirs = [
       "/home"
@@ -343,6 +414,11 @@ in
       "/var/lib/transmission"
       "/var/lib/sddm"
     ];
+    # services.freshrss.enable = true;
+    # services.freshrss.passwordFile = "/persist/freshrss-password";
+    services.miniflux.enable = true;
+    services.nixseparatedebuginfod2.enable = true;
+    services.miniflux.adminCredentialsFile = "/persist/miniflux-admin";
     users.users.${config.services.borgbackup.repos.uknas.user}.home = "/home/borg";
     services.borgbackup.repos = {
       uknas = {
@@ -415,7 +491,7 @@ in
       };
     };
     swapDevices = lib.mkForce [
-      { device = swap; }
+      { device = swap; discardPolicy = "once"; }
     ];
     boot.resumeDevice = swap;
   };
